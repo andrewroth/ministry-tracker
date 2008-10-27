9abe88b042995a3aa4e9aea9b54e4d36b89338b7
Josh <machomagna@gmail.com>
2008-10-26 22:06:13 -0500

    add feature mind map

---
+++ docs/US_MT_Features.mm
@@ -0,0 +1,64 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1225072323486" ID="Freemind_Link_423731467" MODIFIED="1225072331000" TEXT="US MT">
+<node CREATED="1225072348616" ID="Freemind_Link_1850974071" MODIFIED="1225072390601" POSITION="right" TEXT="Directory">
+<node CREATED="1225072375642" ID="Freemind_Link_824642278" MODIFIED="1225072421948" TEXT="The directory serves as a versatile launching point for much functionality"/>
+<node CREATED="1225072422729" ID="Freemind_Link_468059266" MODIFIED="1225072444823" TEXT="Current Functionality:">
+<node CREATED="1225072445491" ID="Freemind_Link_1959187729" MODIFIED="1225072455486" TEXT="Search (name or email)"/>
+<node CREATED="1225072463766" ID="Freemind_Link_867567450" MODIFIED="1225072476436" TEXT="Link to individual profile"/>
+</node>
+<node CREATED="1225072494629" ID="Freemind_Link_1918264172" MODIFIED="1225072624760" TEXT="Future Functioanilty:">
+<node CREATED="1225072501766" ID="Freemind_Link_493248509" MODIFIED="1225072513536" TEXT="Advanced search (year in school, custom attributes)"/>
+<node CREATED="1225072514941" ID="Freemind_Link_839264959" MODIFIED="1225072519435" TEXT="Store queries"/>
+<node CREATED="1225072520054" ID="Freemind_Link_1842686329" MODIFIED="1225072524272" TEXT="Store lists"/>
+<node CREATED="1225072524854" ID="Freemind_Link_1916777532" MODIFIED="1225072536835" TEXT="Initiate mass email"/>
+</node>
+<node CREATED="1225075944645" ID="Freemind_Link_304296724" MODIFIED="1225075951536" TEXT="Export to excel">
+<node CREATED="1225075952149" ID="Freemind_Link_1680648038" MODIFIED="1225075971744" TEXT="Creates a csv dump that the user can then take and use elsewhere."/>
+</node>
+</node>
+<node CREATED="1225072351667" ID="Freemind_Link_462728678" MODIFIED="1225072352973" POSITION="right" TEXT="Groups">
+<node CREATED="1225072630529" ID="_" MODIFIED="1225072635885" TEXT="Bible Studies"/>
+<node CREATED="1225073368828" ID="Freemind_Link_1065860633" MODIFIED="1225073372210" TEXT="Ministry Teams"/>
+<node CREATED="1225073372791" ID="Freemind_Link_1652204627" MODIFIED="1225073387547" TEXT="A &quot;Group&quot; is a fairly arbitrary and expandable concept."/>
+<node CREATED="1225073392565" ID="Freemind_Link_452863336" MODIFIED="1225073400110" TEXT="Groups have 3 levels of membership:">
+<node CREATED="1225073400440" ID="Freemind_Link_1871708803" MODIFIED="1225073404847" TEXT="Group Leaders"/>
+<node CREATED="1225073405453" ID="Freemind_Link_703772349" MODIFIED="1225073410584" TEXT="Group Members"/>
+<node CREATED="1225073411228" ID="Freemind_Link_1340297302" MODIFIED="1225073418001" TEXT="Interested Persons">
+<node CREATED="1225073418479" ID="Freemind_Link_1425566779" MODIFIED="1225073478873" TEXT="These are people who may have expressed an interested in attending your bible study, but haven&apos;t made it yet. You don&apos;t want to count them as &quot;involved&quot;, but you don&apos;t want to loose track of them either" VSHIFT="6"/>
+</node>
+</node>
+</node>
+<node CREATED="1225072353541" ID="Freemind_Link_1901117215" MODIFIED="1225072363635" POSITION="right" TEXT="Customizeable">
+<node CREATED="1225075986525" ID="Freemind_Link_176713530" MODIFIED="1225076060312" TEXT="Customizeability is a core value of the system. For a global system to be useful to a wide range of people, the ability to customize things is key."/>
+</node>
+<node CREATED="1225072582453" ID="Freemind_Link_854558664" MODIFIED="1225072586573" POSITION="left" TEXT="Profile">
+<node CREATED="1225073489866" ID="Freemind_Link_602233892" MODIFIED="1225073495335" TEXT="Involvement">
+<node CREATED="1225074757039" ID="Freemind_Link_1217020191" MODIFIED="1225074779273" TEXT="Lists this person&apos;s involvement activities">
+<node CREATED="1225074779664" ID="Freemind_Link_834384717" MODIFIED="1225074789407" TEXT="Summer Projects Attended (if available)"/>
+<node CREATED="1225074789651" ID="Freemind_Link_1811172454" MODIFIED="1225074798839" TEXT="Conferences Attended (if available)"/>
+<node CREATED="1225074790851" ID="Freemind_Link_423711179" MODIFIED="1225075539195" TEXT="STINTS Attended (if available)"/>
+<node CREATED="1225074791726" ID="Freemind_Link_1945790162" MODIFIED="1225074834133" TEXT="Campus Directors can manually add other events (Fall Retreat, etc)"/>
+</node>
+</node>
+<node CREATED="1225073495978" ID="Freemind_Link_1105138753" MODIFIED="1225073498297" TEXT="Training">
+<node CREATED="1225075219113" ID="Freemind_Link_617129787" MODIFIED="1225075239770" TEXT="Can be populated with set curriculum by upper leadership"/>
+<node CREATED="1225075240538" ID="Freemind_Link_1645057405" MODIFIED="1225075251633" TEXT="Local level can also add their own curriculum"/>
+<node CREATED="1225075252188" ID="Freemind_Link_1038776192" MODIFIED="1225075263659" TEXT="Keeps track of what materials have been covered, when, and by whom"/>
+</node>
+<node CREATED="1225073500953" ID="Freemind_Link_102921992" MODIFIED="1225073505672" TEXT="Contact Info">
+<node CREATED="1225075184038" ID="Freemind_Link_1345799871" MODIFIED="1225075197283" TEXT="International Friendly address fields"/>
+</node>
+<node CREATED="1225073506190" ID="Freemind_Link_1964955755" MODIFIED="1225073510409" TEXT="Extra Attributes">
+<node CREATED="1225073511328" ID="Freemind_Link_619358073" MODIFIED="1225073545021" TEXT="Each ministry is able to define extra profile attributes that only show up for people in their ministries. This allows the tool to stay &quot;General&quot;, while still catering to specific needs"/>
+</node>
+</node>
+<node CREATED="1225075369050" ID="Freemind_Link_827647244" MODIFIED="1225075383961" POSITION="left" TEXT="Heiarchical">
+<node CREATED="1225075430913" ID="Freemind_Link_1602369645" MODIFIED="1225075457644" TEXT="Each ministry is under a parent level, with CCCI at the top of everything."/>
+<node CREATED="1225075458275" ID="Freemind_Link_1369164671" MODIFIED="1225075487607" TEXT="This allows information to flow up the &quot;food chain&quot;, allowing for useful statistics at the upper levels of leadership."/>
+</node>
+<node CREATED="1225075645163" ID="Freemind_Link_406866231" MODIFIED="1225075684882" POSITION="left" TEXT="Import">
+<node CREATED="1225075685413" ID="Freemind_Link_523258951" MODIFIED="1225075705119" TEXT="Bulk Import functionality makes it easier for ministries to migrate from existing systems."/>
+</node>
+</node>
+</map>
