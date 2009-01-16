048ceb41d4d3a4b51dbab38da5afe5886df57a64
Josh <machomagna@gmail.com>
2009-01-15 21:41:05 -0600

    remove unnneeded migration

--- db/migrate/024_add_ministry_id_to_user_group.rb
+++
@@ -1,9 +0,0 @@
-class AddMinistryIdToUserGroup < ActiveRecord::Migration
-  def self.up
-    add_column :user_groups, :ministry_id, :integer
-  end
-
-  def self.down
-    remove_column :user_groups, :ministry_id
-  end
-end
