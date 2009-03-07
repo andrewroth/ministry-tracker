b3bbd7584df64169590fff43274631426dc790b9
Josh Starcher <josh.starcher@gmail.com>
2009-03-07 15:52:51 -0600

    group types need to load mappings

--- app/models/group_type.rb
+++ app/models/group_type.rb
@@ -1,5 +1,7 @@
 class GroupType < ActiveRecord::Base
+  load_mappings
   belongs_to :ministry
+  has_many :groups
   validates_presence_of :group_type, :ministry_id
   
   def unsuitability_metric(id)
