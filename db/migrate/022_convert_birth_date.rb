f590275dbe3e0a3b5dbaadb54b556e460887fd2e
Andrew Roth <andrewroth@gmail.com>
2009-04-15 16:59:18 -0400

    check for Date type already

--- db/migrate/022_convert_birth_date.rb
+++ db/migrate/022_convert_birth_date.rb
@@ -1,8 +1,8 @@
 class ConvertBirthDate < ActiveRecord::Migration
   def self.up
     Person.find(:all).each do |p|
-      p.birth_date = Date.parse(p.birth_date).to_s unless p.birth_date.nil?
-      p.graduation_date = Date.parse(p.graduation_date).to_s unless p.graduation_date.nil?
+      p.birth_date = Date.parse(p.birth_date).to_s unless p.birth_date.nil? || p.birth_date.is_a?(Date)
+      p.graduation_date = Date.parse(p.graduation_date).to_s unless p.graduation_date.nil? || p.graduation_date.is_a?(Date)
       p.save
     end
     change_column :people, :birth_date, :date
@@ -10,7 +10,7 @@ class ConvertBirthDate < ActiveRecord::Migration
   end
 
   def self.down    
-        change_column :people, :birth_date, :string
-        change_column :people, :graduation_date, :string
+    change_column :people, :birth_date, :string
+    change_column :people, :graduation_date, :string
   end
 end
