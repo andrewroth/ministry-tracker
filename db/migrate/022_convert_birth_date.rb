082f93ce89fc7c147322184e6188030ce477b777
Andrew Roth <andrewroth@gmail.com>
2009-04-15 21:06:23 -0400

    more rigorous checking for whether person records need to change

--- db/migrate/022_convert_birth_date.rb
+++ db/migrate/022_convert_birth_date.rb
@@ -1,12 +1,29 @@
+require 'ruby-debug'
+
 class ConvertBirthDate < ActiveRecord::Migration
   def self.up
+
+    change_types = false
+
     Person.find(:all).each do |p|
-      p.birth_date = Date.parse(p.birth_date).to_s unless p.birth_date.nil? || p.birth_date.is_a?(Date)
-      p.graduation_date = Date.parse(p.graduation_date).to_s unless p.graduation_date.nil? || p.graduation_date.is_a?(Date)
-      p.save
+      birth_is_date = p.birth_date.is_a?(Date)
+      birth_is_nil = p.birth_date.nil?
+      p.birth_date = Date.parse(p.birth_date).to_s unless birth_is_date || birth_is_nil
+
+      grad_is_date = p.graduation_date.is_a?(Date)
+      grad_is_nil = p.graduation_date.nil?
+      p.graduation_date = Date.parse(p.graduation_date).to_s unless grad_is_date || grad_is_nil
+
+      if (!birth_is_date && !birth_is_nil) || (!grad_is_date && !grad_is_nil)
+        p.save
+        change_types = true
+      end
     end
-    change_column :people, :birth_date, :date
-    change_column :people, :graduation_date, :date
+
+    if change_types
+      change_column :people, :birth_date, :date
+      change_column :people, :graduation_date, :date
+    end
   end
 
   def self.down    
