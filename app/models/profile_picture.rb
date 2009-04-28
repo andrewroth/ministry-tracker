38fe7d44d40e38cc3e7f320717be341be6f93fed
Andrew Roth <andrewroth@gmail.com>
2009-04-28 17:08:39 -0400

    check for amazon_s3.yml existing and not being in test mode before using s3 storage mode

--- app/models/profile_picture.rb
+++ app/models/profile_picture.rb
@@ -4,10 +4,15 @@ class ProfilePicture < ActiveRecord::Base
     index _(:id)
     index _(:person_id)
   end
+
+  def self.storage_mode
+    !Rails.env.test? && File.exists?(Rails.root.join('config','amazon_s3.yml')) ? :s3 : :file_system
+  end
+
   belongs_to :person, :class_name => "Person", :foreign_key => _("person_id")
 
   has_attachment :content_type => :image, 
-                 :storage => :s3, 
+                 :storage => storage_mode, 
                  :max_size => 10.megabytes,
                  :resize_to => '400x400>',
                  :thumbnails => {:mini => '50x50>',
