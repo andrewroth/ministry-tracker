fad7450ff59532f50ecb8788ced9ec93707cfde0
Andrew Roth <andrewroth@gmail.com>
2009-05-15 23:59:58 -0400

    Daniel - this test was crashing.  Should have either def test_synctax or test description do syntax

--- test/unit/correspondence_type_test.rb
+++ test/unit/correspondence_type_test.rb
@@ -2,12 +2,8 @@ require 'test_helper'
 
 class CorrespondenceTypeTest < ActiveSupport::TestCase
   fixtures CorrespondenceType.table_name, EmailTemplate.table_name
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
 
-  test_create_new_type do
+  def test_create_new_type
     ct = CorrespondenceType.new
   end
 end
