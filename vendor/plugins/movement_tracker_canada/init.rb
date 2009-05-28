438d40fd89f2d8c87bf210e0af34fb5851bb8f52
Andrew Roth <andrewroth@gmail.com>
2009-05-28 16:42:03 -0400

    remove scripts as they will be in the base config of this branch

--- vendor/plugins/movement_tracker_canada/init.rb
+++
@@ -1,4 +0,0 @@
-require 'ftools'
-
-File.copy File.join(File.dirname(__FILE__), 'mappings.yml'), Rails.root.join('config')
-File.copy File.join(File.dirname(__FILE__), 'deploy.rb'), Rails.root.join('config')
