4c99624d29e62024530fca55ecff1968f9438234
Ryan Janzen <lagattack@gmail.com>
2009-05-29 15:17:24 -0400

    Ensures that a new GCX user is assigned to a 'Student' role in the 'No Ministy' ministry.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -216,10 +216,11 @@ class ApplicationController < ActionController::Base
         @ministry = session[:ministry_id] ? Ministry.find(session[:ministry_id]) : @person.ministries.first
         # If we didn't get a ministry out of that, check for a ministry through campus
         @ministry ||= @person.campus_involvements.first.ministry unless @person.campus_involvements.empty? 
-        # If we still don't have a ministry, this person hasn't been assigned a campus. 
-        # Give them the default ministry
-        @ministry ||= Ministry.find_or_create_by_name("No Ministry")
-        
+
+        # If we still don't have a ministry, this person hasn't been assigned a campus.
+        # Looks like we have to give them some dummy information. BUG 1857 
+        @ministry ||= associate_person_with_dummy_ministry(@person)
+
         # if we currently have the top level ministry, great. If not, get it.
         if @ministry.root?
           @root_ministry = @ministry
@@ -247,5 +248,21 @@ class ApplicationController < ActionController::Base
     def adjust_format_for_facebook
       request.format = :facebook if iphone_request?
     end
+
+private
+  # Ensures that the _person_ is involved in the 'No Ministry' ministry
+  # BUG 1857
+  def associate_person_with_dummy_ministry(person)
+    # Ensure the 'No Ministry' ministry exists
+    ministry = Ministry.find_or_create_by_name("No Ministry")
+    mr = MinistryRole.find_by_name('Student')
     
+    mi = MinistryInvolvement.new
+    mi.person_id = person.id
+    mi.ministry_id = ministry.id
+    mi.ministry_role_id = mr.id
+    mi.save
+    
+    ministry
+  end
 end
