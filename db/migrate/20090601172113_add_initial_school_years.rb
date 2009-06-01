8c2d664d23700867104003b07d111ed174d128a1
Suraj Reddy <sixstring@gmail.com>
2009-06-01 14:43:16 -0400

    Added Initial School Year migration

---
+++ db/migrate/20090601172113_add_initial_school_years.rb
@@ -0,0 +1,22 @@
+class AddInitialSchoolYears < ActiveRecord::Migration
+  def self.up
+    # Create 5 School Years
+    if RAILS_ENV == 'development'
+      for i in 1..5 do
+        SchoolYear.create! :name => 'Year '+i.to_s(),
+                           :level => 'Level '+i.to_s(),
+                           :position => i              
+      end
+    end
+  end
+
+  def self.down
+    # Delete the 5 created school years.
+    if RAILS_ENV == 'development'
+      for i in 1..5 do
+        SchoolYear.find_by_name('Year '+i.to_s).destroy
+      end
+    end
+  end
+  
+end
