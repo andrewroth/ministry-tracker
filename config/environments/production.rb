eff96a2416aa00f8190ae8055822570d1abc629b
Andrew Roth <andrewroth@gmail.com>
2009-06-02 16:32:04 -0400

    handle model loading from any rake task, not just db: ones in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,5 +23,5 @@ config.action_controller.perform_caching             = true
 
 # Threadsafe breaks model loading from migrations - see 
 # https://rails.lighthouseapp.com/projects/8994-ruby-on-rails/tickets/2506-models-are-not-loaded-in-migrations-when-configthreadsafe-is-set
-config.threadsafe! unless (File.basename($0) == "rake" && !ARGV.grep(/db:/).empty?)
+config.threadsafe! unless (File.basename($0) == "rake" && !ARGV.grep(/[\w]+:/).empty?)
 
