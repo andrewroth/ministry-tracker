5aaa7dae68b0383030ef86883d5dafb0d960c777
Andrew Roth <andrewroth@gmail.com>
2009-06-03 15:37:16 -0400

    fix for lib paths not loading in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,5 +23,9 @@ config.action_controller.perform_caching             = true
 
 # Threadsafe breaks model loading from migrations - see 
 # https://rails.lighthouseapp.com/projects/8994-ruby-on-rails/tickets/2506-models-are-not-loaded-in-migrations-when-configthreadsafe-is-set
-config.threadsafe! unless (File.basename($0) == "rake" && !ARGV.grep(/[\w]+:/).empty?)
+# ---
+# June 3, 2009 - threadsafe removes dependency loading, which is needed
+# for emu to load the canada plugin models
+# ---
+#config.threadsafe! unless (File.basename($0) == "rake" && !ARGV.grep(/[\w]+:/).empty?)
 
