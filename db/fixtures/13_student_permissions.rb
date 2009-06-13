babeb9f476cca41851c674d6829031671c7486f0
Andrew Roth <andrewroth@gmail.com>
2009-06-13 19:46:54 -0400

    add groups show permission

--- db/fixtures/13_student_permissions.rb
+++ db/fixtures/13_student_permissions.rb
@@ -11,3 +11,8 @@ MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
   rp.ministry_role_id = ministry_role_id
   rp.permission_id = p_id :groups, :join
 end
+
+MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
+  rp.ministry_role_id = ministry_role_id
+  rp.permission_id = p_id :groups, :show
+end
