f46a939c8779ccbb50ce986a011dceb0360d611c
Suraj Reddy <sixstring@gmail.com>
2009-06-13 19:29:19 -0400

    Added Inheritance for Permission Seeds

--- db/fixtures/13_student_permissions.rb
+++ db/fixtures/13_student_permissions.rb
@@ -1,9 +1,6 @@
-ministry_role_id = MinistryRole.find_by_name('Student').id
+require "seed_fu_helper"
 
-# Helper: Find the Permission ID for a given controller and action
-def p_id(controller, action)
-  Permission.find_by_controller_and_action(controller.to_s,action.to_s).id
-end
+ministry_role_id = set_or_inherit_ministry_role_id 'Student'
 
 MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
   rp.ministry_role_id = ministry_role_id
---
+++ db/fixtures/14_involved_student_permissions.rb
@@ -0,0 +1,5 @@
+require "seed_fu_helper"
+
+ministry_role_id = set_or_inherit_ministry_role_id 'Involved Student'
+
+inherit_seed('13_student_permissions')
