860efafb47988ecc8fd90518ade93bfa19b35668
Andrew Roth <andrewroth@gmail.com>
2009-06-13 17:34:40 -0400

    add position to roles

--- db/fixtures/09_staff_roles.rb
+++ db/fixtures/09_staff_roles.rb
@@ -4,16 +4,19 @@ StaffRole.seed(:name) do |sr|
   sr.name = 'Campus Coordinator'
   sr.description = ''
   sr.ministry = ministry
+  sr.position = 5
 end
 
 StaffRole.seed(:name) do |sr|
   sr.name = 'Missionary'
   sr.description = ''
   sr.ministry = ministry
+  sr.position = 6
 end
 
 StaffRole.seed(:name) do |sr|
   sr.name = 'Admin'
   sr.description = ''
   sr.ministry = ministry
+  sr.position = 7
 end
--- db/fixtures/10_student_roles.rb
+++ db/fixtures/10_student_roles.rb
@@ -4,22 +4,26 @@ StudentRole.seed(:name) do |sr|
   sr.name = 'Ministry Leader'
   sr.description = 'a student who oversees a campus, eg LINC leader'
   sr.ministry = ministry
+  sr.position = 1
 end
 
 StudentRole.seed(:name) do |sr|
   sr.name = 'Student Leader'
   sr.description = ''
   sr.ministry = ministry
+  sr.position = 2
 end
 
 StudentRole.seed(:name) do |sr|
   sr.name = 'Involved Student'
   sr.description = 'we are saying has been attending events for at least 6 months'
   sr.ministry = ministry
+  sr.position = 3
 end
 
 StudentRole.seed(:name) do |sr|
   sr.name = 'Student'
   sr.description = ''
   sr.ministry = ministry
+  sr.position = 4
 end
