b086b1534516fb525fc4372f3362768a6695496b
Andrew Roth <andrewroth@gmail.com>
2009-07-14 02:22:31 -0400

    made only Canadian campuses be imported to our ministry

--- db/fixtures/17_campuses.rb
+++ db/fixtures/17_campuses.rb
@@ -1,5 +1,5 @@
 c4c = Ministry.find_by_name 'Campus for Christ'
-for c in Campus.all
+for c in Country.find_by_country_desc('Canada').states.collect(&:campuses).flatten # only cdn campuses
   MinistryCampus.seed(:campus_id, :ministry_id) do |mc|
     mc.campus_id = c.id
     mc.ministry_id = c4c.id
