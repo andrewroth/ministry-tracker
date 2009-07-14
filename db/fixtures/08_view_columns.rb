1296353d87b6e08d500b14f8ed49a249c6a010f3
Andrew Roth <andrewroth@gmail.com>
2009-07-14 02:13:01 -0400

    remove url column, was skipping the wrong id

--- db/fixtures/08_view_columns.rb
+++ db/fixtures/08_view_columns.rb
@@ -38,24 +38,19 @@ ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.column_id = 8
 end
 
-ViewColumn.seed(:view_id, :column_id) do |vc|
-  vc.view_id = 1
-  vc.column_id = 9
-end
+# emu doesn't have a Website column
 
 ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.view_id = 1
   vc.column_id = 10
 end
 
-# emu doesn't have a Website column
-
 ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.view_id = 1
-  vc.column_id = 12
+  vc.column_id = 11
 end
 
 ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.view_id = 1
-  vc.column_id = 13
+  vc.column_id = 12
 end
