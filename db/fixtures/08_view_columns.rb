96eca9d3eecf3a80e04cc436a1a58f55cce45774
Andrew Roth <andrewroth@gmail.com>
2009-07-22 14:53:18 -0400

    reduced number of columns in view so it actually fits

--- db/fixtures/08_view_columns.rb
+++ db/fixtures/08_view_columns.rb
@@ -1,55 +1,65 @@
+# first name
 ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.view_id = 1
   vc.column_id = 1
 end
 
+# last name
 ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.view_id = 1
   vc.column_id = 2
 end
 
+=begin
+# street
 ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.view_id = 1
   vc.column_id = 3
 end
+=end
 
+# city
 ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.view_id = 1
   vc.column_id = 4
 end
 
+# state
 ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.view_id = 1
   vc.column_id = 5
 end
 
+=begin
+# zip
 ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.view_id = 1
   vc.column_id = 6
 end
+=end
 
+# email
 ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.view_id = 1
   vc.column_id = 7
 end
 
+=begin
+# picture
 ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.view_id = 1
   vc.column_id = 8
 end
+=end
 
+# campus
 ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.view_id = 1
   vc.column_id = 9
 end
 
+# school year
 ViewColumn.seed(:view_id, :column_id) do |vc|
   vc.view_id = 1
   vc.column_id =10
 end
-
-# emu doesn't have a Website column
-#ViewColumn.seed(:view_id, :column_id) do |vc|
-#  vc.view_id = 1
-#  vc.column_id =11
-#end
