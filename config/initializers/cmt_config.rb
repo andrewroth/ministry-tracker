fb6770b3a1a31ad9b50654c4b4ee6eb2bafb0167
Andrew Roth <andrewroth@gmail.com>
2009-08-07 02:43:19 -0400

    disable default permissions

--- config/initializers/cmt_config.rb
+++ config/initializers/cmt_config.rb
@@ -46,7 +46,7 @@ module Cmt
 
     # When no Permission object is found for a controller/action,
     # does the user have permission
-    :permissions_granted_by_default => true,
+    :permissions_granted_by_default => false,
     
     # When true, hides timetable impact dropdown in group_types form
     :disable_group_timetable_impact => true,
