d1cbf1dc44f4b1d8c1c0a9ec64969bd74b7d7eae
Andrew Roth <andrewroth@gmail.com>
2009-08-10 17:11:49 -0400

    added a few other campuses that were missing from the seed migration

--- db/fixtures/development/03_campuses.rb
+++ db/fixtures/development/03_campuses.rb
@@ -4,3 +4,31 @@ Campus.seed(:name) do |c|
   c.state = 'ON'
   c.country = 'CA'
 end
+
+Campus.seed(:name) do |c|
+  c.name = 'University of Calgary'
+  c.abbrv = 'UC'
+  c.state = 'AB'
+  c.country = 'CA'
+end
+
+Campus.seed(:name) do |c|
+  c.name = 'University of British Columbia'
+  c.abbrv = 'UBC'
+  c.state = 'BC'
+  c.country = 'CA'
+end
+
+Campus.seed(:name) do |c|
+  c.name = 'Simon Fraser University'
+  c.abbrv = 'SFU'
+  c.state = 'BC'
+  c.country = 'CA'
+end
+
+Campus.seed(:name) do |c|
+  c.name = 'University of Saskatchewan'
+  c.abbrv = 'UofS'
+  c.state = 'SK'
+  c.country = 'CA'
+end
