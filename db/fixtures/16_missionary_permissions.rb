b91ab8875a58f75afbf6dffed71f2b3879d8f5b8
Andrew Roth <andrewroth@gmail.com>
2009-08-11 16:57:34 -0400

    allow staff to be able to edit people's roles and profiles

--- db/fixtures/16_missionary_permissions.rb
+++ db/fixtures/16_missionary_permissions.rb
@@ -48,3 +48,13 @@ MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
   rp.ministry_role_id = ministry_role_id
   rp.permission_id = p_id :staff, :index
 end
+
+MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
+  rp.ministry_role_id = ministry_role_id
+  rp.permission_id = p_id :people, :edit
+end
+
+MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
+  rp.ministry_role_id = ministry_role_id
+  rp.permission_id = p_id :people, :show
+end
