5ac75df233fa96f5dda39d881ae2f173d730b6f4
Andrew Roth <andrewroth@gmail.com>
2009-08-12 13:41:34 -0400

    clean up state and address mappings a bit; use the parent class CimHrdbAddress to avoid duplication (DRY); use associations whenever possible rather than finding directly

--- vendor/plugins/movement_tracker_canada/app/models/cim_hrdb_current_address.rb
+++ vendor/plugins/movement_tracker_canada/app/models/cim_hrdb_current_address.rb
@@ -1,28 +1,7 @@
 class CimHrdbCurrentAddress < CimHrdbAddress
   load_mappings
-  belongs_to :country, :class_name => 'Country', :foreign_key => :local_person_country_id
-  belongs_to :province, :class_name => 'State', :foreign_key => :local_person_province_id
-  
-  def state
-    if self.person_local_province_id
-      State.find_by_province_id(self.person_local_province_id).try(:province_shortDesc)
-    end
-  end
-
-  def state=(v)
-    self.person_local_province_id = State.find(:first, :conditions => { :province_shortDesc => v}).try(:id)
-    self.save!
-  end
-  
-  def country
-    Country.find_by_country_id(country_id) ? Country.find_by_country_id(country_id).country_shortDesc : ''
-  end
-  
-  def country=(val) 
-    self.person_local_country_id = Country.find(:first, :conditions => {:country_shortDesc => val}).try(:country_id)
-    self.save!
-  end
-  
+  belongs_to :country_ref, :class_name => 'Country', :foreign_key => 'person_local_country_id'
+  belongs_to :state_ref, :class_name => 'State', :foreign_key => 'person_local_province_id'
   
   doesnt_implement_attributes :address2 => '', :email_validated => false
 
--- vendor/plugins/movement_tracker_canada/app/models/cim_hrdb_permanent_address.rb
+++ vendor/plugins/movement_tracker_canada/app/models/cim_hrdb_permanent_address.rb
@@ -1,27 +1,8 @@
 class CimHrdbPermanentAddress < CimHrdbAddress
   load_mappings
-  belongs_to :country
-  belongs_to :province, :class_name => 'State'
+  belongs_to :country_ref, :class_name => 'Country', :foreign_key => 'country_id'
+  belongs_to :state_ref, :class_name => 'State', :foreign_key => 'province_id'
   
-  
-  def state
-    province.province_shortDesc if province
-  end
-
-  def state=(v)
-    self.province_id = State.find_by_province_id(:first, :conditions => { :province_shortDesc => v}).try(:id)
-    self.save!
-  end
-  
-  def country
-    Country.find_by_country_id(person_local_country_id) ? Country.find_by_country_id(person_local_country_id).country_shortDesc : ''
-  end
-  
-  def country=(val) 
-    self.country_id = Country.find(:first, :conditions => {:country_shortDesc => val}).try(:country_id)
-    self.save!
-  end
-
   doesnt_implement_attributes :address2 => '', :email_validated => false
 
   def address_type() 'permanent' end
