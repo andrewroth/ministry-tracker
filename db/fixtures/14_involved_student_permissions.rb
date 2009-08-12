b1ad0e55dd59f24b6347414b2431a609cc5f3766
Eric Bendall <ebendall@gmail.com>
2009-08-11 22:00:46 -0400

    useless duplicate seed

--- db/fixtures/14_involved_student_permissions.rb
+++ db/fixtures/14_involved_student_permissions.rb
@@ -49,10 +49,6 @@ MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
   rp.permission_id = p_id :people, :new
 end
 
-MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
-  rp.ministry_role_id = ministry_role_id
-  rp.permission_id = p_id :people, :new
-end
 
 MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
   rp.ministry_role_id = ministry_role_id
