286ed54cad19c577aaff90e13e1a951fba1e9d3f
Andrew Roth <andrewroth@gmail.com>
2009-08-13 19:46:42 -0400

    changed my mind and decided it's a security risk to have students even able to see group details, since emails are on there; but involved students and above can

--- db/fixtures/14_involved_student_permissions.rb
+++ db/fixtures/14_involved_student_permissions.rb
@@ -11,6 +11,11 @@ end
 
 MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
   rp.ministry_role_id = ministry_role_id
+  rp.permission_id = p_id :groups, :show
+end
+
+MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
+  rp.ministry_role_id = ministry_role_id
   rp.permission_id = p_id :groups, :compare_timetables
 end
 
