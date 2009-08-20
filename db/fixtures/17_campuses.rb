bd0ab9e7d0d83980d5c2a2135967fcf4a419c45d
Eric Bendall <ebendall@gmail.com>
2009-08-20 15:30:24 -0400

    permission seeds planted

--- db/fixtures/17_campuses.rb
+++
@@ -1,7 +0,0 @@
-c4c = Ministry.find_by_name 'Campus for Christ'
-for c in Country.find_by_country_desc('Canada').states.collect(&:campuses).flatten # only cdn campuses
-  MinistryCampus.seed(:campus_id, :ministry_id) do |mc|
-    mc.campus_id = c.id
-    mc.ministry_id = c4c.id
-  end
-end
