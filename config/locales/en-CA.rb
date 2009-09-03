5f0a03260e9f75ec21c9ad30f77102266894e9d9
Andrew Roth <andrewroth@gmail.com>
2009-09-03 17:12:13 -0400

    try putting google analytics in again

--- config/locales/en-CA.rb
+++ config/locales/en-CA.rb
@@ -21,17 +21,18 @@
     :help_header => "Support",
     :alternate_phone => 'Cell Phone',
     
-    :analytics => "<script type=\"text/javascript\">
-var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");
-document.write(unescape(\"%3Cscript src=\'\" + gaJsHost + \"/google-analytics.com/ga.js\' type=\'text/javascript\'/%3E%3C/script%3E\"));
+    :analytics => %|
+<script type="text/javascript">
+    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+    try {
+    var pageTracker = _gat._getTracker("UA-2437988-20");
+    pageTracker._trackPageview();
+    } catch(err) {}
 </script>
-<script type=\"text/javascript\">
-try {
-var pageTracker = _gat._getTracker(\"UA-2437988-20\");
-pageTracker._trackPageview();
-} catch(err) {}</script>",
-    
-    
+|,
     :date => {
       :formats => {
         :default => "%m/%d/%Y",
