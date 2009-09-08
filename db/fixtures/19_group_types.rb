c40f2a5e6a9631fb1cf03b74dabf0b38d349b273
Andrew Roth <andrewroth@gmail.com>
2009-09-08 12:05:05 -0400

    the proper list of emu fixtures was lost

--- db/fixtures/19_group_types.rb
+++ db/fixtures/19_group_types.rb
@@ -1,6 +1,26 @@
-GroupType.seed(:group_type) do |gt|
-  gt.group_type = I18n.t :Bible_Study, :default => 'Bible Study' 
-  gt.public = true
-  gt.mentor_priority = true
-  gt.ministry_id = 1
+c4c = Ministry.find_by_name 'Campus for Christ'
+
+GroupType.seed(:ministry_id, :group_type) do |gt|
+  gt.ministry_id = c4c.id
+  gt.group_type = 'Discipleship Group (DG)'
+end
+
+GroupType.seed(:ministry_id, :group_type) do |gt|
+  gt.ministry_id = c4c.id
+  gt.group_type = 'Movement Development Area (MDA)'
+end
+
+GroupType.seed(:ministry_id, :group_type) do |gt|
+  gt.ministry_id = c4c.id
+  gt.group_type = 'Prayer Group'
+end
+
+GroupType.seed(:ministry_id, :group_type) do |gt|
+  gt.ministry_id = c4c.id
+  gt.group_type = 'Servant Team'
+end
+
+GroupType.seed(:ministry_id, :group_type) do |gt|
+  gt.ministry_id = c4c.id
+  gt.group_type = 'Other'
 end
