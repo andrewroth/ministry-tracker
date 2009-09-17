6054e232dc142bda468198f3e470df21ddf429bd
Andrew Roth <andrewroth@gmail.com>
2009-09-17 00:59:53 -0400

    turns out the join on person -> access -> viewer was wrong, resulting in wrong last login times

--- db/fixtures/07_columns.rb
+++ db/fixtures/07_columns.rb
@@ -91,6 +91,6 @@ Column.seed(:title) do |c|
   c.from_clause = 'User'
   c.select_clause = 'last_login'
   c.source_model = 'Access'
-  c.source_column = 'person_id'
+  c.source_column = 'viewer_id'
   c.foreign_key = 'viewer_id'
 end
