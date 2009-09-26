a37684d20617a3251dee65576cee8df7387d49a7
Andrew Roth <andrewroth@gmail.com>
2009-09-26 15:00:13 -0400

    add Canadian ministry teams

--- db/fixtures/01_ministries.rb
+++ db/fixtures/01_ministries.rb
@@ -1,9 +1,144 @@
-Ministry.seed(:name) do |m|
-  m.name = 'Power to Change Ministries'
-  m.parent_id = nil
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Power to Change Ministries'
+  ministry.parent_id = nil
 end
 
-Ministry.seed(:name) do |m|
-  m.name = 'Campus for Christ'
-  m.parent_id = 1
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Campus for Christ'
+  ministry.parent_id = 1
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Campus Alumni'
+  ministry.parent_id = 2
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Ev Strat'
+  ministry.parent_id = 2
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Global Impact'
+  ministry.parent_id = 2
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Korean'
+  ministry.parent_id = 2
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'National'
+  ministry.parent_id = 2
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'National Ops'
+  ministry.parent_id = 2
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'New Scholars'
+  ministry.parent_id = 2
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Other'
+  ministry.parent_id = 2
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Ontario Regional'
+  ministry.parent_id = 2
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Eastern Regional'
+  ministry.parent_id = 2
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Western Regional'
+  ministry.parent_id = 2
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Quebec'
+  ministry.parent_id = 2
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Guelph'
+  ministry.parent_id = 11
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Mac'
+  ministry.parent_id = 11
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Toronto Metro'
+  ministry.parent_id = 11
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Ottawa Metro'
+  ministry.parent_id = 11
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'UWO'
+  ministry.parent_id = 11
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Waterloo'
+  ministry.parent_id = 11
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Halifax Metro'
+  ministry.parent_id = 12
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'U of A'
+  ministry.parent_id = 13
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'U of C'
+  ministry.parent_id = 13
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'U of M'
+  ministry.parent_id = 13
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'U of S'
+  ministry.parent_id = 13
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Vancouver Metro'
+  ministry.parent_id = 13
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Laval'
+  ministry.parent_id = 14
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Montreal Metro'
+  ministry.parent_id = 14
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.name = 'Sherbrooke'
+  ministry.parent_id = 14
 end
