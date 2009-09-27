ac94ebf423aca1a6eeec71c519a7b8c87c2c34f8
Andrew Roth <andrewroth@gmail.com>
2009-09-27 15:29:37 -0400

    update fixtures with real team info

--- db/fixtures/01_ministries.rb
+++ db/fixtures/01_ministries.rb
@@ -1,144 +1,173 @@
 Ministry.seed(:name) do |ministry|
+  ministry.id = 1
   ministry.name = 'Power to Change Ministries'
-  ministry.parent_id = nil
+  ministry.ministries_count = 1
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 2
   ministry.name = 'Campus for Christ'
-  ministry.parent_id = 1
+  ministry.ministries_count = 12
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 3
   ministry.name = 'Campus Alumni'
-  ministry.parent_id = 2
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 4
   ministry.name = 'Ev Strat'
-  ministry.parent_id = 2
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 5
   ministry.name = 'Global Impact'
-  ministry.parent_id = 2
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 6
   ministry.name = 'Korean'
-  ministry.parent_id = 2
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 7
   ministry.name = 'National'
-  ministry.parent_id = 2
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 8
   ministry.name = 'National Ops'
-  ministry.parent_id = 2
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 9
   ministry.name = 'New Scholars'
-  ministry.parent_id = 2
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 10
   ministry.name = 'Other'
-  ministry.parent_id = 2
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 11
   ministry.name = 'Ontario Regional'
-  ministry.parent_id = 2
+  ministry.ministries_count = 6
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 12
   ministry.name = 'Eastern Regional'
-  ministry.parent_id = 2
+  ministry.ministries_count = 1
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 13
   ministry.name = 'Western Regional'
-  ministry.parent_id = 2
+  ministry.ministries_count = 5
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 14
   ministry.name = 'Quebec'
-  ministry.parent_id = 2
+  ministry.ministries_count = 3
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 15
   ministry.name = 'Guelph'
-  ministry.parent_id = 11
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 16
   ministry.name = 'Mac'
-  ministry.parent_id = 11
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
-  ministry.name = 'Toronto Metro'
-  ministry.parent_id = 11
-end
-
-Ministry.seed(:name) do |ministry|
+  ministry.id = 17
   ministry.name = 'Ottawa Metro'
-  ministry.parent_id = 11
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 18
+  ministry.name = 'Toronto Metro'
+  ministry.ministries_count = 0
+end
+
+Ministry.seed(:name) do |ministry|
+  ministry.id = 19
   ministry.name = 'UWO'
-  ministry.parent_id = 11
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 20
   ministry.name = 'Waterloo'
-  ministry.parent_id = 11
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 21
   ministry.name = 'Halifax Metro'
-  ministry.parent_id = 12
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 22
   ministry.name = 'U of A'
-  ministry.parent_id = 13
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 23
   ministry.name = 'U of C'
-  ministry.parent_id = 13
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 24
   ministry.name = 'U of M'
-  ministry.parent_id = 13
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 25
   ministry.name = 'U of S'
-  ministry.parent_id = 13
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 26
   ministry.name = 'Vancouver Metro'
-  ministry.parent_id = 13
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 27
   ministry.name = 'Laval'
-  ministry.parent_id = 14
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 28
   ministry.name = 'Montreal Metro'
-  ministry.parent_id = 14
+  ministry.ministries_count = 0
 end
 
 Ministry.seed(:name) do |ministry|
+  ministry.id = 29
   ministry.name = 'Sherbrooke'
-  ministry.parent_id = 14
+  ministry.ministries_count = 0
 end
