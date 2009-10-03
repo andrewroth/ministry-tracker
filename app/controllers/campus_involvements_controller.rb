cc314c22c06a3790caf28d4ef72a3576f2b2440f
Andrew Roth <andrewroth@gmail.com>
2009-10-03 16:47:52 -0400

    can do all CRUD operations on campuses

--- app/controllers/campus_involvements_controller.rb
+++ app/controllers/campus_involvements_controller.rb
@@ -2,6 +2,27 @@ class CampusInvolvementsController < ApplicationController
   def index
   end
 
+  def new
+    @campus_involvement = @person.campus_involvements.new
+    @campuses = Campus.all
+    @school_years = SchoolYear.all
+  end
+
+  def create
+    conditions = params[:campus_involvement].merge(:ministry_id => get_ministry.id)
+    @campus_involvement = @person.campus_involvements.find :first, :conditions => {
+      :campus_id => conditions[:campus_id], :ministry_id => conditions[:ministry_id] 
+    }
+    if @campus_involvement
+      @campus_involvement.update_attributes conditions
+      @updated = true
+    else
+      @campus_involvement = @person.campus_involvements.create! conditions
+    end
+    @campuses = Campus.all
+    @school_years = SchoolYear.all
+  end
+
   def destroy
     # if @person.campus_involvements.count > 1
       @campus_involvement = CampusInvolvement.find(params[:id])
@@ -21,5 +42,24 @@ class CampusInvolvementsController < ApplicationController
     #        end
          # end
     # end
+  end
+
+  def edit
+    @campus_involvement = @person.campus_involvements.find params[:id]
+    @campuses = Campus.all
+    @school_years = SchoolYear.all
+  end
+
+  def update
+    @campus_involvement = @person.campus_involvements.find params[:id]
+    params[:campus_involvement][:ministry_id] = get_ministry.id
+    params[:campus_involvement][:person_id] = @person.id
+    if ci2 = CampusInvolvement.find(:first, :conditions => {
+      :campus_id => params[:campus_id], :ministry_id => params[:ministry_id]
+    })
+      @campus_involvement.destroy # otherwise will get duplicate key error
+      @campus_involvement = ci2
+    end
+    @campus_involvement.update_attributes params[:campus_involvement]
   end
 end
