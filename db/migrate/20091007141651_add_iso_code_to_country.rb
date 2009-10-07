1f844ca2ebbee548126dca0ccfee31101c34887a
Josh Starcher <josh@josh-starchers-mac-pro.local>
2009-10-07 09:57:28 -0500

    add an iso_code column to countries, and make CmtGeo use it

---
+++ db/migrate/20091007141651_add_iso_code_to_country.rb
@@ -0,0 +1,14 @@
+class AddIsoCodeToCountry < ActiveRecord::Migration
+  def self.up
+    add_column :countries, :iso_code, :string
+    change_column_default :countries, :closed, 0
+    Carmen::COUNTRIES.each do |name, code|
+      c = Country.find_by_country(name)
+      c.update_attribute(:iso_code, code) if c
+    end
+  end
+
+  def self.down
+    remove_column :countries, :iso_code
+  end
+end
