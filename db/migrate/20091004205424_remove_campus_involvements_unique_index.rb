61bd1ad0506396288a1c2bf9e1bbf44861e3e74f
Andrew Roth <andrewroth@gmail.com>
2009-10-07 18:16:32 -0400

    fix updating campus_involvements; add uniq index

--- db/migrate/20091004205424_remove_campus_involvements_unique_index.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCampusInvolvementsUniqueIndex < ActiveRecord::Migration
-  def self.up
-    remove_index :campus_involvements, [:person_id, :campus_id]
-  end
-
-  def self.down
-    add_index :campus_involvements, [:person_id, :campus_id], :unique => true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091004205424) do
+ActiveRecord::Schema.define(:version => 20091007141651) do
 
   create_table "addresses", :force => true do |t|
     t.integer "person_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(:version => 20091004205424) do
 
   add_index "campus_involvements", ["campus_id"], :name => "index_campus_involvements_on_campus_id"
   add_index "campus_involvements", ["ministry_id"], :name => "index_campus_involvements_on_ministry_id"
+  add_index "campus_involvements", ["person_id", "campus_id", "end_date"], :name => "index_campus_involvements_on_p_id_and_c_id_and_end_date", :unique => true
   add_index "campus_involvements", ["person_id"], :name => "index_campus_involvements_on_person_id"
 
   create_table "campuses", :force => true do |t|
