091fd8b5caa48b75d0213a991b19e3b931a1a00f
Andrew Roth <andrewroth@gmail.com>
2009-10-27 01:21:09 -0400

    move people in to subministries

--- db/fixtures/08_view_columns.rb
+++ db/fixtures/08_view_columns.rb
@@ -1,77 +1,79 @@
+v_id = Ministry.find_by_name("Campus for Christ").views.first.id
+
 # first name
 ViewColumn.seed(:view_id, :column_id) do |vc|
-  vc.view_id = 1
+  vc.view_id = v_id
   vc.column_id = 1
 end
 
 # last name
 ViewColumn.seed(:view_id, :column_id) do |vc|
-  vc.view_id = 1
+  vc.view_id = v_id
   vc.column_id = 2
 end
 
 =begin
 # street
 ViewColumn.seed(:view_id, :column_id) do |vc|
-  vc.view_id = 1
+  vc.view_id = v_id
   vc.column_id = 3
 end
 =end
 
 # city
 ViewColumn.seed(:view_id, :column_id) do |vc|
-  vc.view_id = 1
+  vc.view_id = v_id
   vc.column_id = 4
 end
 
 # state
 ViewColumn.seed(:view_id, :column_id) do |vc|
-  vc.view_id = 1
+  vc.view_id = v_id
   vc.column_id = 5
 end
 
 =begin
 # zip
 ViewColumn.seed(:view_id, :column_id) do |vc|
-  vc.view_id = 1
+  vc.view_id = v_id
   vc.column_id = 6
 end
 =end
 
 # email
 ViewColumn.seed(:view_id, :column_id) do |vc|
-  vc.view_id = 1
+  vc.view_id = v_id
   vc.column_id = 7
 end
 
 =begin
 # picture
 ViewColumn.seed(:view_id, :column_id) do |vc|
-  vc.view_id = 1
+  vc.view_id = v_id
   vc.column_id = 8
 end
 =end
 
 # campus
 ViewColumn.seed(:view_id, :column_id) do |vc|
-  vc.view_id = 1
+  vc.view_id = v_id
   vc.column_id = 9
 end
 
 # school year
 ViewColumn.seed(:view_id, :column_id) do |vc|
-  vc.view_id = 1
+  vc.view_id = v_id
   vc.column_id = 10
 end
 
 # last login
 #ViewColumn.seed(:view_id, :column_id) do |vc|
-#  vc.view_id = 1
+#  vc.view_id = v_id
 #  vc.column_id = 11
 #end
 
 # on pulse since
 ViewColumn.seed(:view_id, :column_id) do |vc|
-  vc.view_id = 1
+  vc.view_id = v_id
   vc.column_id = 12
 end
