735b72ed5c8ba8f0ba61ced6bfcbbfa2f25ae0ac
Andrew Roth <andrewroth@gmail.com>
2010-01-09 00:47:20 -0500

    strange, thought these fixtures were already in; in any case, enable emailing

--- db/fixtures/17_staff.rb
+++ db/fixtures/17_staff.rb
@@ -1,21 +1,20 @@
 require "seed_fu_helper"
 
-ministry_role_id = set_or_inherit_ministry_role_id 'Student Leader'
+ministry_role_id = set_or_inherit_ministry_role_id 'Staff'
 
-inherit_seed('14_involved_student_permissions')
+inherit_seed('16_ministry_leader_permissions')
 
-# will be released version 1.01
 MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
   rp.ministry_role_id = ministry_role_id
-  rp.permission_id = p_id :emails, :new
+  rp.permission_id = p_id :people, :directory
 end
 
 MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
   rp.ministry_role_id = ministry_role_id
-  rp.permission_id = p_id :emails, :index
+  rp.permission_id = p_id :people, :search
 end
 
 MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
   rp.ministry_role_id = ministry_role_id
-  rp.permission_id = p_id :people, :email
+  rp.permission_id = p_id :people, :advanced
 end
--- db/fixtures/18_staff_team.rb
+++ db/fixtures/18_staff_team.rb
@@ -2,7 +2,7 @@ require "seed_fu_helper"
 
 ministry_role_id = set_or_inherit_ministry_role_id 'Staff Team'
 
-inherit_seed('16_ministry_leader_permissions')
+inherit_seed('17_staff')
 
 MinistryRolePermission.seed(:ministry_role_id, :permission_id) do |rp|
   rp.ministry_role_id = ministry_role_id
