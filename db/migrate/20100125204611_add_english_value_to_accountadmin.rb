332791d04117c0a6ce79719a09b8aa722e8b47a5
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-01-26 13:32:29 -0500

    Refactor 20100125204611_add_english_value_to_accountadmin.rb

--- db/migrate/20100125204611_add_english_value_to_accountadmin.rb
+++ db/migrate/20100125204611_add_english_value_to_accountadmin.rb
@@ -1,14 +1,14 @@
 class AddEnglishValueToAccountadmin < ActiveRecord::Migration
 
-  MODELS = ["AccountadminAccesscategory", "AccountadminAccessgroup", "AccountadminAccountgroup", "AccountadminLanguage"]
+  MODELS = [AccountadminAccesscategory, AccountadminAccessgroup, AccountadminAccountgroup, AccountadminLanguage]
 
   def self.up
     begin
 
       MODELS.each do |model|
-        add_column model.constantize.table_name, :english_value, :text
+        add_column model.table_name, :english_value, :text
 
-        model.constantize.all.each do |row|
+        model.all.each do |row|
           english = SiteMultilingualLabel.all(:select => SiteMultilingualLabel.__(:label),
             :conditions => ["#{SiteMultilingualLabel.__(:key)} = ? and #{SiteMultilingualLabel.__(:label)} not like ?", row.key, "[%]%"] )
           row.english_value = english.first.label
@@ -19,14 +19,14 @@ class AddEnglishValueToAccountadmin < ActiveRecord::Migration
     rescue Exception => e
       puts e.message
       MODELS.each do |model|
-        remove_column model.constantize.table_name, :english_value
+        remove_column model.table_name, :english_value
       end
     end
   end
 
   def self.down
     MODELS.each do |model|
-      remove_column model.constantize.table_name, :english_value
+      remove_column model.table_name, :english_value
     end
   end
 end
