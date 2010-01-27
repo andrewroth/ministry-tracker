f9739fb3dfd81080f3eb9452a1a81529a8071366
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-01-27 13:43:29 -0500

    Remove begin/rescue

--- db/migrate/20100125204611_add_english_value_to_accountadmin.rb
+++ db/migrate/20100125204611_add_english_value_to_accountadmin.rb
@@ -3,25 +3,18 @@ class AddEnglishValueToAccountadmin < ActiveRecord::Migration
   MODELS = [AccountadminAccesscategory, AccountadminAccessgroup, AccountadminAccountgroup, AccountadminLanguage]
 
   def self.up
-    begin
 
-      MODELS.each do |model|
-        add_column model.table_name, :english_value, :text
+    MODELS.each do |model|
+      add_column model.table_name, :english_value, :text
 
-        model.all.each do |row|
-          english = SiteMultilingualLabel.all(:select => SiteMultilingualLabel.__(:label),
-            :conditions => ["#{SiteMultilingualLabel.__(:key)} = ? and #{SiteMultilingualLabel.__(:label)} not like ?", row.key, "[%]%"] )
-          row.english_value = english.first.label
-          row.save
-        end
-      end
-
-    rescue Exception => e
-      puts e.message
-      MODELS.each do |model|
-        remove_column model.table_name, :english_value
+      model.all.each do |row|
+        english = SiteMultilingualLabel.all(:select => SiteMultilingualLabel.__(:label),
+          :conditions => ["#{SiteMultilingualLabel.__(:key)} = ? and #{SiteMultilingualLabel.__(:label)} not like ?", row.key, "[%]%"] )
+        row.english_value = english.first.label
+        row.save
       end
     end
+
   end
 
   def self.down
