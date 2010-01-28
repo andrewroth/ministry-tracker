069c1c0df156a0cdad6a1fb37d368002d5e47bf4
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-01-28 14:36:22 -0500

    Fix migration not working on Accountgroup label_long

--- db/migrate/20100125204611_add_english_value_to_accountadmin.rb
+++ db/migrate/20100125204611_add_english_value_to_accountadmin.rb
@@ -1,17 +1,26 @@
+#require 'ruby-debug'
+
 class AddEnglishValueToAccountadmin < ActiveRecord::Migration
 
-  MODELS = [AccountadminAccesscategory, AccountadminAccessgroup, AccountadminAccountgroup, AccountadminLanguage]
+  #MODELS = [AccountadminAccesscategory, AccountadminAccessgroup, AccountadminAccountgroup, AccountadminLanguage]
+  MODELS = [AccountadminAccountgroup]
 
   def self.up
 
+#    debugger
+    
     MODELS.each do |model|
       add_column model.table_name, :english_value, :text
-
+      model.reset_column_information
+      puts model.name
       model.all.each do |row|
+        puts row.id.to_s
         english = SiteMultilingualLabel.all(:select => SiteMultilingualLabel.__(:label),
           :conditions => ["#{SiteMultilingualLabel.__(:key)} = ? and #{SiteMultilingualLabel.__(:label)} not like ?", row.key, "[%]%"] )
+        puts english.first.label
         row.english_value = english.first.label
         row.save
+        puts row.english_value
       end
     end
 
--- vendor/plugins/movement_tracker_canada/app/models/accountadmin_accountgroup.rb
+++ vendor/plugins/movement_tracker_canada/app/models/accountadmin_accountgroup.rb
@@ -3,7 +3,7 @@ class AccountadminAccountgroup < ActiveRecord::Base
 
   has_many :users
 
-  validates_presence_of _(:key), _(:label_long)
+  validates_presence_of _(:key)
   validates_uniqueness_of _(:key)
   validates_length_of _(:key), :maximum => 50
   validates_length_of _(:label_long), :maximum => 50
