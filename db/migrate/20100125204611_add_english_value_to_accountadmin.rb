69b47b88ca01206b602b896de57f851893bff389
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-01-29 11:23:32 -0500

    remove puts from migration

--- db/migrate/20100125204611_add_english_value_to_accountadmin.rb
+++ db/migrate/20100125204611_add_english_value_to_accountadmin.rb
@@ -7,15 +7,11 @@ class AddEnglishValueToAccountadmin < ActiveRecord::Migration
     MODELS.each do |model|
       add_column model.table_name, :english_value, :text
       model.reset_column_information
-      puts model.name
       model.all.each do |row|
-        puts row.id.to_s
         english = SiteMultilingualLabel.all(:select => SiteMultilingualLabel.__(:label),
           :conditions => ["#{SiteMultilingualLabel.__(:key)} = ? and #{SiteMultilingualLabel.__(:label)} not like ?", row.key, "[%]%"] )
-        puts english.first.label
         row.english_value = english.first.label
         row.save
-        puts row.english_value
       end
     end
 
