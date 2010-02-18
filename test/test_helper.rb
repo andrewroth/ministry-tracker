df823a1c20268097e6ca9627679cded023ef364a
Andrew Roth <andrewroth@gmail.com>
2010-02-18 16:22:04 -0500

    remove fixtures load all and add logger

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -34,7 +34,11 @@ class ActiveSupport::TestCase
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
-  fixtures :all
+  #fixtures :all
+
+  def logger
+    RAILS_DEFAULT_LOGGER
+  end
 
   # Add more helper methods to be used by all tests here...
   def login(username = 'josh.starcher@example.com')
