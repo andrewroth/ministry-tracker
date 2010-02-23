93cd496fdf8605bab932157f4f44e2f35a765123
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-02-23 12:58:39 -0500

    Make Is Active check box show for people who aren't staff and add them to staff if it's checked

--- vendor/plugins/movement_tracker_canada/app/controllers/cim_hrdb_staff_controller.rb
+++ vendor/plugins/movement_tracker_canada/app/controllers/cim_hrdb_staff_controller.rb
@@ -2,10 +2,24 @@ class CimHrdbStaffController < ApplicationController
   unloadable
   layout 'manage'
 
-  def change
+  def change_or_add
     @cim_hrdb_person = Person.find(params[:person_id])
-    @cim_hrdb_staff = @cim_hrdb_person.cim_hrdb_staff
 
+    # create a new entry in the staff table if one does not exist
+    if @cim_hrdb_person.cim_hrdb_staff.nil?
+      @cim_hrdb_staff = CimHrdbStaff.new
+      @cim_hrdb_staff.person_id = @cim_hrdb_person.id
+      @cim_hrdb_staff.is_active = 0
+      
+      unless @cim_hrdb_staff.save
+        flash[:notice] = 'OOPS! Could not change is active!'
+        render(:update) { |page| page.redirect_to edit_cim_hrdb_person_path(@cim_hrdb_person)}
+      end
+    end
+
+    @cim_hrdb_staff = CimHrdbStaff.all(:conditions => {:person_id => @cim_hrdb_person.id}).first
+
+    # flip the is_active boolean
     @cim_hrdb_staff.is_active = @cim_hrdb_staff.is_active == 0 ? 1 : 0
 
     if @cim_hrdb_staff.save
