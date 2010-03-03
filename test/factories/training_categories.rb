69d4e6f5e07c59d298aef4dab2afafac3f1cb493
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-03-03 16:50:02 -0500

    Make TrainingCategory factories singletons

--- test/factories/training_categories.rb
+++ test/factories/training_categories.rb
@@ -1,11 +1,11 @@
-Factory.define :trainingcategory_1, :class => TrainingCategory do |t|
+Factory.define :trainingcategory_1, :class => TrainingCategory, :singleton => true do |t|
   t.id '1'
   t.name 'Four Laws'
   t.position '1'
   t.ministry_id '1'
 end
 
-Factory.define :trainingcategory_2, :class => TrainingCategory do |t|
+Factory.define :trainingcategory_2, :class => TrainingCategory, :singleton => true do |t|
   t.id '2'
   t.position '2'
   t.name 'Empty Category'
