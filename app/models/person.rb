cbc924abb1f88db74c9ee59afa916fd7b07071b9
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-03-05 10:30:12 -0500

    Databases

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,8 @@
 class Person < ActiveRecord::Base
   load_mappings
   include Common::Person
+
+  # Summer Projects
+  has_many :summer_project_applications, :order =>  "#{SummerProjectApplication.table_name}.#{_(:created_at, :summer_project_application)}"
+  has_many :summer_projects, :through => :summer_project_applications, :order => "#{SummerProject.table_name}.#{_(:created_at, :summer_project)} desc"
 end
