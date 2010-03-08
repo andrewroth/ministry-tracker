13e99da36a0aa7ecae7d38011536f2c09415163e
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-03-08 16:15:41 -0500

    Move code out of CDM

--- app/models/ministry.rb
+++ app/models/ministry.rb
@@ -3,9 +3,26 @@ class Ministry < ActiveRecord::Base
   include Common::Core::Ministry
 
   has_many :views, :order => View.table_name + '.' + _(:title, 'view'), :dependent => :destroy
-
+  has_many :groups, :dependent => :destroy
+  has_many :group_types, :class_name => "GroupType", :foreign_key => _(:ministry_id, :group_type)
+  has_many :custom_attributes, :dependent => :destroy
+  has_many :profile_questions, :dependent => :destroy
+  has_many :involvement_questions, :dependent => :destroy
+  has_many :training_categories, :class_name => "TrainingCategory", :foreign_key => _(:ministry_id, :training_category), :order => _(:position, :training_category), :dependent => :destroy
+  has_many :training_questions, :order => "activity", :dependent => :destroy
   after_create :create_first_view
 
+
+  def all_training_categories
+    @all_training_categories ||= Array.wrap(ancestors.collect(&:training_categories)).flatten.uniq
+    return @all_training_categories
+  end
+
+  def all_training_questions
+    @all_training_questions ||= Array.wrap(ancestors.collect(&:training_questions)).flatten.uniq
+    return @all_training_questions
+  end
+  
   def create_first_view
     # copy the default ministry's first view if possible
     if Cmt::CONFIG[:default_ministry_name] &&
