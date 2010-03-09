6915b90f4b369535790b971217df8225ddcf518e
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-03-09 17:45:42 -0500

    Move code out of CDM into MT

--- app/models/ministry_involvement.rb
+++ app/models/ministry_involvement.rb
@@ -1,4 +1,8 @@
 class MinistryInvolvement < ActiveRecord::Base
   load_mappings
   include Common::Core::MinistryInvolvement
+
+  def new_staff_history
+    ::StaffInvolvementHistory.new :person_id => person_id, :end_date => Date.today, :ministry_role_id => ministry_role_id, :start_date => (last_history_update_date || start_date), :ministry_involvement_id => id, :ministry_id => ministry_id
+  end
 end
