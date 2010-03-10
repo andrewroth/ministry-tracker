812c9df5e15221276d5ea648479a98510dd95bd5
Andrew Roth <andrewroth@gmail.com>
2010-03-10 17:20:16 -0500

    remove factories that are now in CDM

--- test/factories/counties.rb
+++
@@ -1,7 +0,0 @@
-Factory.define :county_1, :class => County do |c|
-  c.id '1'
-end
-
-Factory.define :county_2, :class => County do |c|
-  c.id '2'
-end
--- test/factories/staff.rb
+++
@@ -1,9 +0,0 @@
-Factory.define :staff_1, :class => Staff do |s|
-  s.id '1'
-  s.accountNo '1'
-end
-
-Factory.define :staff_2, :class => Staff do |s|
-  s.id '2'
-  s.accountNo '2'
-end
