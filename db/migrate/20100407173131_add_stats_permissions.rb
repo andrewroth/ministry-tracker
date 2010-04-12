b9c28b28ed2810b64edb5ec6d1ad2ad370544a33
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-04-12 17:12:46 -0400

    Add semester_at_a_glance permissions

--- db/migrate/20100407173131_add_stats_permissions.rb
+++ db/migrate/20100407173131_add_stats_permissions.rb
@@ -19,16 +19,39 @@ class AddStatsPermissions < ActiveRecord::Migration
 #                      { :description => "View Summary by Month", :controller => "regional_team", :action => "summary_by_month" },
 #                      { :description => "View Summary by Campus", :controller => "regional_team", :action => "summary_by_campus" } ]
 
-  NEW_PERMISSIONS = [ { :description => "Submit Weekly Stats Numbers", :controller => "weekly_report", :action => "new" } ]
+  NEW_PERMISSIONS = [ { :description => "View Stats Tab", :controller => "stats", :action => "index" },
+                      { :description => "Submit Weekly Stats Numbers", :controller => "weekly_reports", :action => "new" },
+                      { :description => "View Semester at a Glance report", :controller => "stats", :action => "semester_at_a_glance" } ]
+
+  NEW_MINISTRY_ROLE_PERMISSIONS = [ { :ministry_role_id => 1,  :controller => "stats", :action => "index" },
+                                    { :ministry_role_id => 13, :controller => "stats", :action => "index" },
+                                    { :ministry_role_id => 1,  :controller => "weekly_reports", :action => "new" },
+                                    { :ministry_role_id => 13, :controller => "weekly_reports", :action => "new" },
+                                    { :ministry_role_id => 1,  :controller => "stats", :action => "semester_at_a_glance" },
+                                    { :ministry_role_id => 13, :controller => "stats", :action => "semester_at_a_glance" }]
 
   def self.up
     NEW_PERMISSIONS.each do |permission|
       p = Permission.new(permission)
       p.save
     end
+
+    NEW_MINISTRY_ROLE_PERMISSIONS.each do |mrp|
+      permission_id = Permission.find(:first, :conditions => { :controller => mrp[:controller], :action => mrp[:action] } ).id
+
+      p = MinistryRolePermission.new( {:permission_id => permission_id, :ministry_role_id => mrp[:ministry_role_id] } )
+      p.save
+    end
   end
 
   def self.down
+    NEW_MINISTRY_ROLE_PERMISSIONS.each do |mrp|
+      permission_id = Permission.find(:first, :conditions => { :controller => mrp[:controller], :action => mrp[:action] } ).id
+
+      p = MinistryRolePermission.find(:first, :conditions => {:permission_id => permission_id, :ministry_role_id => mrp[:ministry_role_id] } )
+      p.destroy
+    end
+
     NEW_PERMISSIONS.each do |permission|
       p = Permission.find(:first, :conditions => { :controller => permission[:controller], :action => permission[:action] })
       p.destroy
