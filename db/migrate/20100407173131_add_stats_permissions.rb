9a969784b2c63190ed1006d5f9c4b303adca1293
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-04-23 15:02:45 -0400

    Update stats permissions migration

--- db/migrate/20100407173131_add_stats_permissions.rb
+++ db/migrate/20100407173131_add_stats_permissions.rb
@@ -1,34 +1,15 @@
 class AddStatsPermissions < ActiveRecord::Migration
 
-#  NEW_PERMISSIONS = [ { :description => "View Stats Tab", :controller => "stats", :action => "index" },
-#
-#                      { :description => "View Year Summary", :controller => "all_staff", :action => "year_summary" },
-#                      { :description => "Submit Weekly Stats", :controller => "all_staff", :action => "submit_weekly_stats" },
-#                      { :description => "View Semester at a Glance", :controller => "all_staff", :action => "semester_at_a_glance" },
-#                      { :description => "Select Semester/Campus for Viewing Indicated Decisions (needed for View Indicated Decisions)", :controller => "all_staff", :action => "indicated_decisions" },
-#                      { :description => "Delete Indicated Decision", :controller => "all_staff", :action => "delete" },
-#                      { :description => "Edit Indicated Decision", :controller => "all_staff", :action => "edit" },
-#                      { :description => "View Indicated Decisions", :controller => "all_staff", :action => "decisions" },
-#
-#                      { :description => "View Monthly Summary by Campus", :controller => "campus_directors", :action => "monthly_summary_by_campus" },
-#
-#                      { :description => "View How People Came to Know Christ", :controller => "national_team", :action => "how_people_prayed_to_receive_christ" },
-#                      { :description => "View Indicated Decisions", :controller => "national_team", :action => "indicated_decisions" },
-#
-#                      { :description => "View Summary by Week", :controller => "regional_team", :action => "summary_by_week" },
-#                      { :description => "View Summary by Month", :controller => "regional_team", :action => "summary_by_month" },
-#                      { :description => "View Summary by Campus", :controller => "regional_team", :action => "summary_by_campus" } ]
-
   NEW_PERMISSIONS = [{ :description => "View Stats Tab", :controller => "stats", :action => "index" },
-                     { :description => "Submit Weekly Stats Numbers", :controller => "weekly_reports", :action => "new" },
-                     { :description => "View Semester at a Glance report", :controller => "stats", :action => "semester_at_a_glance" }]
+                     { :description => "Submit Weekly Stats", :controller => "weekly_reports", :action => "new" },
+                     { :description => "Submit Semester Stats", :controller => "semester_reports", :action => "new" }]
 
   NEW_MINISTRY_ROLE_PERMISSIONS = [{ :ministry_role_id => 1,  :controller => "stats", :action => "index" },
                                    { :ministry_role_id => 13, :controller => "stats", :action => "index" },
                                    { :ministry_role_id => 1,  :controller => "weekly_reports", :action => "new" },
                                    { :ministry_role_id => 13, :controller => "weekly_reports", :action => "new" },
-                                   { :ministry_role_id => 1,  :controller => "stats", :action => "semester_at_a_glance" },
-                                   { :ministry_role_id => 13, :controller => "stats", :action => "semester_at_a_glance" }]
+                                   { :ministry_role_id => 1,  :controller => "semester_reports", :action => "new" },
+                                   { :ministry_role_id => 13, :controller => "semester_reports", :action => "new" }]
 
   def self.up
     NEW_PERMISSIONS.each do |permission|
