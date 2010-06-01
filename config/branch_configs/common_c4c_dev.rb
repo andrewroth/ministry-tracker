c169c75873b1c946de14f59fe84c4a35dec278e2
Andrew Roth <andrewroth@gmail.com>
2010-06-01 13:13:54 -0400

    add configs

---
+++ config/branch_configs/common_c4c_dev.rb
@@ -0,0 +1,6 @@
+module Common
+  SERVER = "c4c"
+  APP = "pulse"
+  STAGE = "dev"
+  DISABLE_MOONSHINE = true
+end
---
+++ config/branch_configs/common_c4c_prod.rb
@@ -0,0 +1,6 @@
+module Common
+  SERVER = "c4c"
+  APP = "pulse"
+  STAGE = "prod"
+  DISABLE_MOONSHINE = true
+end
