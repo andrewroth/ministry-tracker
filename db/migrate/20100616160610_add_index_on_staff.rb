3dc4fbf32a6a91166c967b58ee981b1f1af0a0c1
Andrew Roth <andrewroth@gmail.com>
2010-06-16 12:41:52 -0400

    oops dont want staff_id in the index

--- db/migrate/20100616160610_add_index_on_staff.rb
+++ db/migrate/20100616160610_add_index_on_staff.rb
@@ -1,9 +1,9 @@
 class AddIndexOnStaff < ActiveRecord::Migration
   def self.up
-    add_index Staff.table_name, [ :staff_id, :person_id ], :unique => true, :name => "unique_staff_person"
+    add_index Staff.table_name, [ :person_id ], :unique => true, :name => "unique_person"
   end
 
   def self.down
-    remove_index Staff.table_name, :name => "unique_staff_person"
+    remove_index Staff.table_name, :name => "unique_person"
   end
 end
