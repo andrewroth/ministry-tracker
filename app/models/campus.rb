3df88c0f278074e8e44ec2c1a4c0440aba1f9040
Andrew Roth <andrewroth@gmail.com>
2010-06-24 17:11:05 -0400

    add default groups for campuses

--- app/models/campus.rb
+++ app/models/campus.rb
@@ -1,5 +1,33 @@
 class Campus < ActiveRecord::Base
   load_mappings
+  include Common::Core::Campus
   include Common::Core::Ca::Campus
   include Legacy::Stats::Core::Campus
+
+  has_many :groups
+  has_many :campus_ministry_groups
+  has_many :collection_groups, :through => :campus_ministry_groups, :class_name => "Group", :source => :group
+
+  # TODO: if we ever support multiple root ministries in one install, ex. AIA and C4C
+  # and IV, then Ministry.default_ministry will have to be changed to all "root" ministries
+  def ensure_campus_ministry_groups_created(ministries = [ Ministry.default_ministry ])
+    ministries.each do | ministry|
+      ministry.group_types.each do |gt|
+        if gt.has_collection_groups
+          cmg = CampusMinistryGroup.find :first, :conditions => [ 
+          "#{_(:campus_id, :campus_ministry_group)} = ? AND #{_(:ministry_id, :campus_ministry_group)} = ?", self.id, ministry.id ]
+          if cmg
+            group = cmg.group
+          else
+            group = gt.groups.new :campus_id => self.id
+          end
+          group.derive_name
+          group.save!
+
+          cmg ||= CampusMinistryGroup.create! :campus => self, :ministry => ministry, :group => group
+        end
+      end
+    end
+  end
+>>>>>>> add default groups for campuses:app/models/campus.rb
 end
--- app/models/group_type.rb
+++ app/models/group_type.rb
@@ -1,7 +1,17 @@
-
 class GroupType < ActiveRecord::Base
   load_mappings
   belongs_to :ministry
   has_many :groups
   validates_presence_of :group_type, :ministry_id
+  after_save :update_collection_groups
+
+  def update_collection_groups
+    if has_collection_groups
+      # TODO: if we ever support multiple root ministries in one install, ex. AIA and C4C
+      # and IV, then Ministry.default_ministry will have to be changed to all "root" ministries
+      Ministry.default_ministry.campus_groups.each do |cg|
+        cg.derive_name(collection_group_name)
+      end
+    end
+  end
 end
