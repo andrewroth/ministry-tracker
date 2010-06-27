980acb43fd45f271deef40f14cd3196f971b3a33
Andrew Roth <andrewroth@gmail.com>
2010-06-26 23:01:37 -0400

    start check for email existing

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -31,32 +31,38 @@ class SignupController < ApplicationController
       step1_info
       render :action => "step1_info"
     else
-      u = User.find_or_create_from_guid_or_email(nil, params[:person][:email], 
+      @user = User.find_or_create_from_guid_or_email(nil, params[:person][:email], 
                                                params[:person][:first_name],
                                                params[:person][:last_name])
-      @person = u.person 
-      ci = @person.campus_involvements.find :first, :conditions => {
-        :campus_id => @primary_campus_involvement.campus_id
-      }
-      if ci
-        ci.update_student_campus_involvement(flash, StudentRole.default_student_role, nil, 
-                                             @primary_campus_involvement.school_year_id,
-                                             @primary_campus_involvement.campus_id)
+      @person = @user.person 
+      unless @user.just_created && @person.just_created
+        @email = params[:person][:email]
+        render :action => :step1_verify
       else
-        ci = @person.campus_involvements.new
-        ci.campus_id = @primary_campus_involvement.campus_id
-        ci.school_year_id = @primary_campus_involvement.school_year_id
-        ci.start_date = Date.today
-        ci.last_history_update_date = Date.today
-        ci.ministry_id = ci.derive_ministry.try(:id)
-        ci.save!
+        ci = @person.campus_involvements.find :first, :conditions => {
+          :campus_id => @primary_campus_involvement.campus_id
+        }
+        if ci
+          ci.update_student_campus_involvement(flash, StudentRole.default_student_role, nil, 
+                                               @primary_campus_involvement.school_year_id,
+                                               @primary_campus_involvement.campus_id)
+        else
+          ci = @person.campus_involvements.new
+          ci.campus_id = @primary_campus_involvement.campus_id
+          ci.school_year_id = @primary_campus_involvement.school_year_id
+          ci.start_date = Date.today
+          ci.last_history_update_date = Date.today
+          ci.ministry_id = ci.derive_ministry.try(:id)
+          ci.save!
+        end
+        session[:signup_person_id] = @person.id
+        session[:signup_campus_id] = @primary_campus_involvement.campus_id
+
+        puts "HOOKHERE2 person.#{@person.object_id} '#{@person.try(:just_created)}' user.#{@user.object_id} '#{@user.try(:just_created)}'"
+
+        redirect_to :action => :step1_group
       end
     end
-
-    session[:signup_person_id] = @person.id
-    session[:signup_campus_id] = @primary_campus_involvement.campus_id
-
-    redirect_to :action => :step1_group
   end
 
   def step1_group
@@ -69,6 +75,11 @@ class SignupController < ApplicationController
     @collection_group = @campus.collection_groups.find :first, 
       :conditions => [ "#{CampusMinistryGroup.__(:ministry_id)} = ?", Ministry.default_ministry ]
     @groups.delete_if { |g| g == @collection_group }
+  end
+
+  def step1_verify_submit
+    # send verification email
+    
   end
 
   protected
