174401f12c7fa0bd0b70edb61ab7e1070c3ffecb
Jacques Robitaille <jacques@jacques-laptop.(none)>
2010-06-30 14:29:52 -0400

    Added permission to view ccci report to team member

---
+++ db/migrate/20100630182603_add_ccci_report_permission_to_team_member.rb
@@ -0,0 +1,27 @@
+class AddCcciReportPermissionToTeamMember < ActiveRecord::Migration
+ NEW_PERMISSIONS = [{ :controller => "stats", :action => "show_ccci_report" }]
+
+  NEW_MINISTRY_ROLE_PERMISSIONS = [{ :ministry_role_name => "Team Member",  :permission_index => 0 }]
+
+
+  def self.up
+    NEW_MINISTRY_ROLE_PERMISSIONS.each do |mrp|
+      p = NEW_PERMISSIONS[mrp[:permission_index]]
+      mr_id = MinistryRole.find(:first, :conditions => { :name => mrp[:ministry_role_name] } ).id
+      permission_id = Permission.find(:first, :conditions => { :controller => p[:controller], :action => p[:action] } ).id
+      if MinistryRolePermission.find(:all, :conditions => {:permission_id => permission_id, :ministry_role_id => mr_id}).empty?
+        MinistryRolePermission.create!({ :permission_id => permission_id, :ministry_role_id => mr_id })
+      end
+    end
+  end
+
+  def self.down
+    NEW_MINISTRY_ROLE_PERMISSIONS.each do |mrp|
+      p = NEW_PERMISSIONS[mrp[:permission_index]]
+      mr_id = MinistryRole.find(:first, :conditions => { :name => mrp[:ministry_role_name] } ).id
+      permission_id = Permission.find(:first, :conditions => { :controller => p[:controller], :action => p[:action] } ).id
+      mrp_list = MinistryRolePermission.find(:all, :conditions => {:permission_id => permission_id, :ministry_role_id => mr_id})
+      mrp_list.each{ |mrp| mrp.destroy }
+    end
+  end
+end
