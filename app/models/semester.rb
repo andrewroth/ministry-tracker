e8e2f0b791d1c94f78576f52943b08e3fbc669dd
Andrew Roth <andrewroth@gmail.com>
2010-08-23 16:57:12 -0400

    also include stats

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,4 +1,5 @@
 class Semester < ActiveRecord::Base
   load_mappings
   include Common::Core::Semester
+  include Legacy::Stats::Semester
 end
--- app/models/year.rb
+++ app/models/year.rb
@@ -1,4 +1,5 @@
 class Year < ActiveRecord::Base
   load_mappings
   include Common::Core::Year
+  include Legacy::Stats::Year
 end
