85fd9659d753be97106d331ebd62aba0dc39ed2c
Andrew Roth <andrewroth@gmail.com>
2010-08-24 15:54:54 -0400

    use table_name

--- db/migrate/20100708013617_create_semesters.rb
+++ db/migrate/20100708013617_create_semesters.rb
@@ -1,19 +1,19 @@
 class CreateSemesters < ActiveRecord::Migration
   def self.up
-    create_table :semesters do |t|
+    create_table Semester.table_name do |t|
       t.integer :year_id
       t.date :start_date
       t.string :desc
 
       t.timestamps
     end
-    add_index :semesters, :year_id
-    add_index :semesters, :start_date
+    add_index Semester.table_name, :year_id
+    add_index Semester.table_name, :start_date
   end
 
   def self.down
-    remove_index :semesters, :year_id
-    remove_index :semesters, :start_date
-    drop_table :semesters
+    remove_index Semester.table_name, :year_id
+    remove_index Semester.table_name, :start_date
+    drop_table Semester.table_name
   end
 end
