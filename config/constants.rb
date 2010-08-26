6a5015115d2e97ee2d410b1b9c613e9e22fd7c8c
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-08-26 09:39:07 -0400

    Change report action names to match permissions

--- config/constants.rb
+++ config/constants.rb
@@ -60,8 +60,8 @@
       :p2c => {:label => "Power to Change Reports", :controller => :stats, :action => :show_p2c_report},
       :ccci => {:label => "CCCI Reports", :controller => :stats, :action => :show_ccci_report},
       :comp => {:label => "Compliance Reports", :controller => :stats, :action => :show_compliance_report},
-      :hpctc => {:label => "How people came to Christ", :controller => :stats, :action => :show_how_people_came_to_christ_report},
-      :story => {:label => "Salvation Story Synopses", :controller => :stats, :action => :show_story_report}
+      :hpctc => {:label => "How people came to Christ", :controller => :stats, :action => :how_people_came_to_christ},
+      :story => {:label => "Salvation Story Synopses", :controller => :stats, :action => :salvation_story_synopses}
     }
   end
   
