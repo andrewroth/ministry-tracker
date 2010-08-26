5ba1ed95b069a235f3ddc51c3d11fce815d0d954
Jacques Robitaille <jacques.robitaille@gmail.com>
2010-08-26 11:46:58 -0400

    added permissions for compliance reports

---
+++ db/migrate/20100826154230_add_permission_for_compliance_reports.rb
@@ -0,0 +1,22 @@
+class AddPermissionForComplianceReports < ActiveRecord::Migration
+ NEW_PERMISSIONS = [{ :description => "Show compliance report", :controller => "stats", :action => "show_compliance_report" }]
+
+  def self.find_permission(permission)
+    Permission.find(:first, :conditions => { :controller => permission[:controller], :action => permission[:action] } )
+  end
+  
+  def self.up
+    NEW_PERMISSIONS.each do |permission|
+      Permission.create!(permission) if find_permission(permission).nil?
+    end
+  end
+
+  def self.down
+
+    NEW_PERMISSIONS.each do |permission|
+      p = find_permission(permission)
+      p.destroy unless p.nil?
+    end
+
+  end
+end
