ba3f60e47b344ae53966d2549f89e1e4399bdd65
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-09-10 16:55:45 -0400

    remove debugging logger info

--- vendor/plugins/eventbright/lib/eventbright/main.rb
+++ vendor/plugins/eventbright/lib/eventbright/main.rb
@@ -52,7 +52,7 @@ module EventBright
   
   class API
     include HTTParty
-    base_uri "https://www.eventbrite.com/json/"
+    base_uri "https://www.eventbrite.com/json/asdf"
     ERROR_404 = "404 Not Found"
     NUM_RETRIES_ON_404 = 3
 
@@ -69,8 +69,7 @@ module EventBright
         end
 
       rescue Exception => e
-#        puts "\tAPI ERROR #{e.message}"
-        Rails.logger.info "\tAPI ERROR #{e.message}"
+        Rails.logger.info "\tEventBright API ERROR \t#{e.message}"
         if e.message == ERROR_404 && retries > 0
           retries -= 1
           retry unless retries <=0
