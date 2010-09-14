3b86370883f9416b3c2355be7ed599cce0c6d992
Jacques Robitaille <jacques.robitaille@gmail.com>
2010-09-14 14:31:36 -0400

    adjusted moving of stats from Reg/Nat

--- db/migrate/20100914175614_move_regional_national_to_other.rb
+++ db/migrate/20100914175614_move_regional_national_to_other.rb
@@ -6,16 +6,23 @@ class MoveRegionalNationalToOther < ActiveRecord::Migration
     Campus.find(:first, :conditions => { :campus_shortDesc => campus[:campus_shortDesc] } )
   end
   
-  def self.up
-    campus_from = find_campus(COPY_FROM_CAMPUS)
-    campus_to = find_campus(COPY_TO_CAMPUS)
+  def self.move_stats(stats_model, campus_from, campus_to)
     unless campus_from.nil? || campus_to.nil?
-      wr_list = WeeklyReport.find :all, :conditions => { :campus_id => campus_from.id }
+      wr_list = stats_model.find :all, :conditions => { :campus_id => campus_from.id }
       wr_list.each do |wr|
         wr.campus_id = campus_to.id
         wr.save!
       end
     end
+  end
+  
+  def self.up
+    campus_from = find_campus(COPY_FROM_CAMPUS)
+    campus_to = find_campus(COPY_TO_CAMPUS)
+    move_stats(WeeklyReport, campus_from, campus_to)
+    move_stats(Prc, campus_from, campus_to)
+    move_stats(MonthlyReport, campus_from, campus_to)
+    move_stats(SemesterReport, campus_from, campus_to)
   end
 
   def self.down
