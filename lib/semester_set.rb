5e6534d44045bc2d2dca52010bd1f08e807392e4
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-10-18 16:23:38 -0400

    fix semester_set for when semesters are really behind the current date

--- lib/semester_set.rb
+++ lib/semester_set.rb
@@ -2,13 +2,14 @@ module SemesterSet
   def set_current_and_next_semester
     @current_semester = Semester.current
     unless @current_semester
-      Semester.create_default_semesters(2)
+      Semester.create_default_semesters(1)
       @current_semester = Semester.current
     end
     
     @next_semester = @current_semester.next_semester
-    unless @next_semester
+    while @next_semester.nil?
       Semester.create_default_semesters(1) # need another year apparently
+      @current_semester = Semester.current
       @next_semester = @current_semester.next_semester
     end
   end
