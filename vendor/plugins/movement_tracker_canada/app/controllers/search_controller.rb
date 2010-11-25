4ecd965950d35c2cc5afffc157040f60e2fc3ee9
Sheldon Dueck <sheldon.dueck@gmail.com>
2010-11-25 11:45:34 -0500

    merge conflicts

--- vendor/plugins/movement_tracker_canada/app/controllers/search_controller.rb
+++ vendor/plugins/movement_tracker_canada/app/controllers/search_controller.rb
@@ -71,7 +71,7 @@ class SearchController < ApplicationController
 
         " AS rank ", "%#{@q}%", "%#{@q}%", session[:search][:search_ministry_name], semester_current.desc, semester_current.next_semester.desc], '')
 
-    Group.paginate(:page => params[:page],
+    groups = Group.paginate(:page => params[:page],
                    :per_page => params[:per_page],
                    
                    :joins => "LEFT JOIN #{Campus.table_name} ON #{Group.__(:campus_id)} = #{Campus.__(:id)} " +
@@ -88,14 +88,17 @@ class SearchController < ApplicationController
                    :select => select_str,
 
                    :conditions => ["#{session[:search][:group_search_limit_condition]} AND (" +
+                                   "involvements IS NOT NULL OR (" +
                                    "#{Group.__(:name, :group)} like ? " +
-                                   "or #{Group.__(:email, :group)} like ? " +
-                                   ")",
-                                   "%#{@q}%", "%#{@q}%"],
+                                   "))",
+                                 
+                                   "%#{@q}%"],
 
                    :order => 'rank DESC, semester_start DESC',
 
                    :group => "#{Group.__(:id)}")
+
+    groups
 
   end
 
@@ -119,37 +122,39 @@ class SearchController < ApplicationController
         " AS rank", "#{@q}%", "#{@q}%", session[:search][:search_ministry_name] ], '')
   
 
-    Person.paginate(:page => params[:page],
-                    :per_page => params[:per_page],
-                    :joins => "LEFT JOIN #{CampusInvolvement.table_name} ON #{CampusInvolvement.__(:person_id)} = #{Person.__(:person_id)} AND #{CampusInvolvement.__(:end_date)} IS NULL " +
-                              "LEFT JOIN #{Campus.table_name} ON #{Campus.__(:campus_id)} = #{CampusInvolvement.__(:campus_id)} " +
-                              "LEFT JOIN #{ProfilePicture.table_name} ON #{ProfilePicture.__(:person_id)} = #{Person.__(:person_id)} " +
-                              "LEFT JOIN #{MinistryInvolvement.table_name} ON #{MinistryInvolvement.__(:person_id)} = #{Person.__(:person_id)} AND #{MinistryInvolvement.__(:end_date)} IS NULL " +
+    people = Person.paginate(:page => params[:page],
+              :per_page => params[:per_page],
+              :joins => "LEFT JOIN #{CampusInvolvement.table_name} ON #{CampusInvolvement.__(:person_id)} = #{Person.__(:person_id)} AND #{CampusInvolvement.__(:end_date)} IS NULL " +
+                        "LEFT JOIN #{Campus.table_name} ON #{Campus.__(:campus_id)} = #{CampusInvolvement.__(:campus_id)} " +
+                        "LEFT JOIN #{ProfilePicture.table_name} ON #{ProfilePicture.__(:person_id)} = #{Person.__(:person_id)} " +
+                        "LEFT JOIN #{MinistryInvolvement.table_name} ON #{MinistryInvolvement.__(:person_id)} = #{Person.__(:person_id)} AND #{MinistryInvolvement.__(:end_date)} IS NULL " +
 
-                              # need this to find their staff roles to see if they are in fact a staff
-                              "LEFT JOIN #{MinistryRole.table_name} ON #{MinistryRole.__(:id)} = #{MinistryInvolvement.__(:ministry_role_id)} AND #{MinistryRole.__(:type)} = 'StaffRole' " +
+                        # need this to find their staff roles to see if they are in fact a staff
+                        "LEFT JOIN #{MinistryRole.table_name} ON #{MinistryRole.__(:id)} = #{MinistryInvolvement.__(:ministry_role_id)} AND #{MinistryRole.__(:type)} = 'StaffRole' " +
 
-                              # need this to find their ministries to display instead of campuses if they're staff, but skip the P2C and C4C ministries
-                              "LEFT JOIN #{Ministry.table_name} ON #{Ministry.__(:id)} = #{MinistryInvolvement.__(:ministry_id)} AND #{Ministry.__(:id)} > 2 " +
+                        # need this to find their ministries to display instead of campuses if they're staff, but skip the P2C and C4C ministries
+                        "LEFT JOIN #{Ministry.table_name} ON #{Ministry.__(:id)} = #{MinistryInvolvement.__(:ministry_id)} AND #{Ministry.__(:id)} > 2 " +
 
-                              "LEFT JOIN #{Timetable.table_name} ON #{Timetable.__(:person_id)} = #{Person.__(:person_id)} " +
-                              "LEFT JOIN #{CimHrdbPersonYear.table_name} ON #{CimHrdbPersonYear.__(:person_id)} = #{Person.__(:person_id)} " +
-                              "LEFT JOIN #{SchoolYear.table_name} ON #{CimHrdbPersonYear.__(:year_id)} = #{SchoolYear.__(:year_id)} ",
+                        "LEFT JOIN #{Timetable.table_name} ON #{Timetable.__(:person_id)} = #{Person.__(:person_id)} " +
+                        "LEFT JOIN #{CimHrdbPersonYear.table_name} ON #{CimHrdbPersonYear.__(:person_id)} = #{Person.__(:person_id)} " +
+                        "LEFT JOIN #{SchoolYear.table_name} ON #{CimHrdbPersonYear.__(:year_id)} = #{SchoolYear.__(:year_id)} ",
 
-                    :select => select_str,
+              :select => select_str,
 
-                    :conditions => ["#{session[:search][:person_search_limit_condition]} AND (" +
-                                    "concat(#{_(:first_name, :person)}, \" \", #{_(:last_name, :person)}) like ? " +
-                                    "or #{_(:first_name, :person)} like ? " +
-                                    "or #{_(:last_name, :person)} like ? " +
-                                    "or #{_(:email, :person)} like ? " +
-                                    "or #{Person.table_name}.#{_(:id, :person)} like ? " +
-                                    ")",
-                                    "#{@q}%", "#{@q}%", "#{@q}%", "%#{@q}%", "%#{@q}%"],
+              :conditions => ["#{session[:search][:person_search_limit_condition]} AND (" +
+                              "concat(#{_(:first_name, :person)}, \" \", #{_(:last_name, :person)}) like ? " +
+                              "or #{_(:first_name, :person)} like ? " +
+                              "or #{_(:last_name, :person)} like ? " +
+                              "or #{_(:email, :person)} like ? " +
+                              "or #{Person.table_name}.#{_(:id, :person)} like ? " +
+                              ")",
+                              "#{@q}%", "#{@q}%", "#{@q}%", "%#{@q}%", "%#{@q}%"],
 
-                    :order => 'rank DESC',
+              :order => 'rank DESC',
 
-                    :group => "#{Person.__(:id)}")
+              :group => "#{Person.__(:id)}")
+
+    people
 
   end
 
@@ -172,7 +177,7 @@ class SearchController < ApplicationController
         " AS rank", "#{@q}%", "#{@q}%", session[:search][:search_ministry_name] ], '')
 
 
-    Person.all(:limit => MAX_NUM_AUTOCOMPLETE_RESULTS,
+    people = Person.all(:limit => MAX_NUM_AUTOCOMPLETE_RESULTS,
                :joins => "LEFT JOIN #{CampusInvolvement.table_name} ON #{CampusInvolvement.__(:person_id)} = #{Person.__(:person_id)} AND #{CampusInvolvement.__(:end_date)} IS NULL " +
                          "LEFT JOIN #{Campus.table_name} ON #{Campus.__(:campus_id)} = #{CampusInvolvement.__(:campus_id)} " +
                          "LEFT JOIN #{ProfilePicture.table_name} ON #{ProfilePicture.__(:person_id)} = #{Person.__(:person_id)} " +
@@ -198,6 +203,8 @@ class SearchController < ApplicationController
                :order => 'rank DESC',
                
                :group => "#{Person.__(:id)}")
+
+    people
              
   end
 
