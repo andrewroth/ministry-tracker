f6282d9b1ea858c98152e5003e9faed699e3a770
Sheldon Dueck <sheldon.dueck@gmail.com>
2011-01-12 10:03:04 -0500

    maybe this will work

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -11,7 +11,7 @@ module FacebookHelper
         });
 
         window.setTimeout(function() {
-          top.location.href="#"; // prevent confusion when navigating from long page to short page by navigating to the top
+          scrollTo(0,0); // prevent confusion when navigating from long page to short page by navigating to the top
           FB.Canvas.setAutoResize(); // auto resize iframe size to prevent scroll bars (must be enabled in the app's settings)
         }, 250);
 
