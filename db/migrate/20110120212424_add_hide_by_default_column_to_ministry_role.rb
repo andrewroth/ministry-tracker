85d85ead21ad6070dceee514b270e4ed3a06bcfb
Sheldon Dueck <sheldon.dueck@gmail.com>
2011-02-03 17:13:05 -0500

    fix remains of merge

--- db/migrate/20110120212424_add_hide_by_default_column_to_ministry_role.rb
+++ db/migrate/20110120212424_add_hide_by_default_column_to_ministry_role.rb
@@ -1,17 +1,9 @@
 class AddHideByDefaultColumnToMinistryRole < ActiveRecord::Migration
   def self.up
-<<<<<<< HEAD
-    add_column MinistryRole.table_name, :hide_by_default, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column MinistryRole.table_name, MinistryRole._(:hide_by_default)
-=======
     add_column MinistryRole.table_name, :hide_by_default, :boolean, :default => false unless MinistryRole.columns_hash.has_key?("hide_by_default")
   end
 
   def self.down
     remove_column MinistryRole.table_name, MinistryRole._(:hide_by_default) if MinistryRole.columns_hash.has_key?("hide_by_default")
->>>>>>> c4c.fix
   end
 end
