9c1d28507770819fade8a5ac3123062471d75d71
Andrew Roth <andrewroth@gmail.com>
2011-02-03 15:02:32 -0500

    more tests pass

--- test/helpers/pat_helper_test.rb
+++ test/helpers/pat_helper_test.rb
@@ -8,9 +8,20 @@ class PatHelperTest < ActionView::TestCase
     setup_people
     setup_campuses
     setup_campus_involvements
+    Factory(:campusinvolvement_8)
+    Factory(:campusinvolvement_9)
+    Factory(:campusinvolvement_10)
+    Factory(:campusinvolvement_11)
+#    Factory(:user_9)
+#    Factory(:user_10)
+#    Factory(:user_11)
     Factory(:project_1)
+    Factory(:project_2)
     Factory(:profile_1)
     Factory(:profile_2)
+    Factory(:profile_3)
+    Factory(:profile_4)
+    Factory(:profile_5)
     Factory(:appln_1)
     Factory(:form_1)
   end
@@ -20,22 +31,22 @@ class PatHelperTest < ActionView::TestCase
   end
 
   def test_project_acceptance_totals
-    results_by_campus, results_by_project = project_acceptance_totals(Campus.all.collect(&:id))
-    assert results_by_campus[nil][:total] == 1
-    assert results_by_campus[nil]["project 1"] == 1
-    assert results_by_project.keys.first == "project 1"
-    assert results_by_project["project 1"][nil] == 1
-    assert results_by_project["project 1"][:total] == 1
+    results_by_campus, results_by_project = project_acceptance_totals(Campus.all.collect(&:id), nil)
+    assert_equal 1, results_by_campus["UCD"][:total]
+    assert_equal 2, results_by_campus["Wy"]["project 2"]
+    assert_equal 1, results_by_project["project 1"]["UCD"]
+    assert_equal 2, results_by_project["project 1"][:total]
+    assert_equal 2, results_by_project["project 2"]["Wy"]
+    assert_equal 2, results_by_project["project 2"][:total]
   end
 
   def test_project_applying_totals
     results_by_campus, results_by_project = project_applying_totals(Campus.all.collect(&:id))
-    assert results_by_campus[nil][:total] == 1
-    assert results_by_campus[nil]["project 1"] == 1
-    assert results_by_project.keys.first == "project 1"
-    assert results_by_project["project 1"][nil] == 1
-    assert results_by_project["project 1"][:total] == 1
+    assert results_by_campus["UCD"][:total] == 1
+    assert_equal 1, results_by_campus["Wy"]["project 2"]
+    assert_equal 1, results_by_project["project 1"]["UCD"]
+    assert_equal 2, results_by_project["project 1"][:total]
+    assert_equal 1, results_by_project["project 2"]["Wy"]
+    assert_equal 1, results_by_project["project 2"][:total]
   end
-
-  
 end
--- test/helpers/training_helper_test.rb
+++ test/helpers/training_helper_test.rb
@@ -1,11 +1,17 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class TrainingHelperTest < ActionView::TestCase
-  # include ApplicationHelper
+  include ApplicationHelper
   # fixtures Person.table_name, TrainingQuestion.table_name
   # 
-  # def test_training_question_field
-  #   assert_tag_in(training_question_field(TrainingQuestion.first, Person.first), :script)
-  # end
+  def setup
+    setup_default_user
+    Factory(:trainingquestion_1)
+  end
+
+  def test_training_question_field
+    @my = Person.first
+    assert_tag_in(training_question_field(TrainingQuestion.first, Person.first), :script)
+  end
 
 end
