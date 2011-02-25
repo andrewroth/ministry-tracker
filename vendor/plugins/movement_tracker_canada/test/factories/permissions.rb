5e5bda0180be63b989a1bc8dea33b08e360bd545
Andrew Roth <andrewroth@gmail.com>
2011-02-25 15:48:58 -0500

    fix Hobbe's conflicts that he committed

--- vendor/plugins/movement_tracker_canada/test/factories/permissions.rb
+++ vendor/plugins/movement_tracker_canada/test/factories/permissions.rb
@@ -26,21 +26,11 @@ Factory.define :permission_4, :class => Permission do |p|
   p.description 'View Directory'
 end
 
-Factory.define :permission_5_sheldon, :class => Permission do |p|
+Factory.define :permission_5, :class => Permission do |p|
   p.id '5'
-<<<<<<< HEAD
   p.action 'update_multiple_roles'
   p.controller 'ministry_involvements'
   p.description 'Update multiple ministry involvement roles'
-end
-
-Factory.define :permission_5_hobbe, :class => Permission do |p|
-  p.id '555' # originally 5
-=======
->>>>>>> c4c.staging.remove_multiple_student_involvements
-  p.action 'add_mentor_to_other'
-  p.controller 'people'
-  p.description 'Add mentor to other'
 end
 
 Factory.define :permission_6, :class => Permission do |p|
