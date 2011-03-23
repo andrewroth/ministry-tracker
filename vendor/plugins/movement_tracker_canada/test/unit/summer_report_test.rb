dfff578a95bf125d6caf7c87333b66fed3c001f5
Sheldon Dueck <sheldon.dueck@gmail.com>
2011-03-23 11:39:44 -0400

    start summer report tests

--- vendor/plugins/movement_tracker_canada/test/unit/summer_report_test.rb
+++ vendor/plugins/movement_tracker_canada/test/unit/summer_report_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class SummerReportTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def setup
+    setup_years
+    setup_months
+    setup_weeks
+    setup_users
+    setup_people
+    setup_default_user
+    setup_summer_reports
+  end
+
+
+  test "validates week exists" do
+    sr = Factory(:summer_report_1)
+
+    sr.summer_report_weeks.destroy_all
+
+    assert_equal false, sr.valid?
   end
 end
