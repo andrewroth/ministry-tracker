342a523b62d9c96434f667eab97998429a975dfc
Sheldon Dueck <sheldon.dueck@gmail.com>
2011-03-23 12:24:19 -0400

    add reviewer

--- vendor/plugins/movement_tracker_canada/test/unit/summer_report_test.rb
+++ vendor/plugins/movement_tracker_canada/test/unit/summer_report_test.rb
@@ -13,11 +13,56 @@ class SummerReportTest < ActiveSupport::TestCase
   end
 
 
-  test "validates week exists" do
+  test "has a week" do
     sr = Factory(:summer_report_1)
 
+    assert_equal true, sr.valid?
     sr.summer_report_weeks.destroy_all
-
     assert_equal false, sr.valid?
+  end
+
+  test "has a reviewer" do
+    sr = Factory(:summer_report_1)
+
+    assert_equal true, sr.valid?
+    sr.summer_report_reviewers.destroy_all
+    assert_equal false, sr.valid?
+  end
+
+  test "approved" do
+    sr = Factory(:summer_report_1)
+
+    assert_equal true, sr.approved?
+    assert_equal SummerReport::STATUS_APPROVED, sr.status
+  end
+
+  test "reviewed" do
+    sr = Factory(:summer_report_1)
+
+    assert_equal true, sr.reviewed?
+  end
+
+  test "disapproved" do
+    sr = Factory(:summer_report_1)
+    r = sr.summer_report_reviewers.first
+    r.reviewed = true
+    r.approved = false
+    r.save
+
+    assert_equal true, sr.disapproved?
+    assert_equal SummerReport::STATUS_DISAPPROVED, sr.status
+  end
+
+  test "waiting" do
+    sr = Factory(:summer_report_1)
+    sr.summer_report_reviewers.each {|r| r.reviewed = nil; r.approved = nil;}
+
+    assert_equal SummerReport::STATUS_WAITING, sr.status
+  end
+
+  test "status style" do
+    sr = Factory(:summer_report_1)
+
+    assert_equal String, sr.status_style
   end
 end
