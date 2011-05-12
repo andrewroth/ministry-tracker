4337de1d06a38df5b2bd2c6295bfa9edf70c0972
Sheldon Dueck <sheldon.dueck@gmail.com>
2011-05-12 17:07:20 -0400

    prevent migration woes

--- db/migrate/20110512155352_create_login_codes.rb
+++ db/migrate/20110512155352_create_login_codes.rb
@@ -1,17 +1,23 @@
 class CreateLoginCodes < ActiveRecord::Migration
   def self.up
-    create_table LoginCode.table_name do |t|
-      t.boolean :acceptable, :default => true
-      t.integer :times_used, :default => 0
-      t.string :code
-      t.datetime :expires_at, :default => nil
-      t.timestamps
+    begin
+      create_table LoginCode.table_name do |t|
+        t.boolean :acceptable, :default => true
+        t.integer :times_used, :default => 0
+        t.string :code
+        t.datetime :expires_at, :default => nil
+        t.timestamps
+      end
+      add_index LoginCode.table_name, :code
+    rescue
     end
-    add_index LoginCode.table_name, :code
   end
 
   def self.down
-    remove_index LoginCode.table_name, :code
-    drop_table LoginCode.table_name
+    begin
+      remove_index LoginCode.table_name, :code
+      drop_table LoginCode.table_name
+    rescue
+    end
   end
 end
