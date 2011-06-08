47943a37783aacc4453ada5d25784c29db69f3c2
Sheldon Dueck <sheldon.dueck@gmail.com>
2011-06-08 15:03:43 -0400

    link bar widget

--- vendor/plugins/movement_tracker_canada/app/controllers/link_bar_controller.rb
+++ vendor/plugins/movement_tracker_canada/app/controllers/link_bar_controller.rb
@@ -6,8 +6,8 @@ class LinkBarController < ApplicationController
   def widget
     @show_staff = false
     @page_wrapper_width = params[:page_wrapper_width] if params[:page_wrapper_width]
+    @active_tab_id = params[:active_tab_id] ? params[:active_tab_id] : ""
     @base_url = base_url
-    @base_url = "https://pulse.campusforchrist.org"
     
     render :layout => false
   end
