b6ace0d77cba5047c8c2fe02733bfdd5075295df
Andrew Roth <andrewroth@gmail.com>
2011-06-10 13:29:44 -0400

    add all_for_grouped_options

--- vendor/plugins/movement_tracker_canada/app/models/month.rb
+++ vendor/plugins/movement_tracker_canada/app/models/month.rb
@@ -4,4 +4,14 @@ class Month < ActiveRecord::Base
   load_mappings
   
   include Legacy::Stats::Month
+
+  def self.all_in_order
+    Month.all(:joins => :year, :order => "year_desc ASC, month_number ASC")
+  end
+
+  def self.all_for_grouped_options
+    Year.all.collect{ |y|
+      [ y.year_desc, y.months.collect{ |m| [ m.month_desc, "m_#{m.id}" ] } ]
+    }
+  end
 end
