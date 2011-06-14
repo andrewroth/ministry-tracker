4d9960cee1f45154e9d767f5f58cfac8c7240b6a
Andrew Roth <andrewroth@gmail.com>
2011-06-14 16:20:59 -0400

    fix misc bugs with global dashboard

--- vendor/plugins/movement_tracker_canada/app/models/month.rb
+++ vendor/plugins/movement_tracker_canada/app/models/month.rb
@@ -14,4 +14,10 @@ class Month < ActiveRecord::Base
       [ y.year_desc, y.months.collect{ |m| [ m.month_desc, "m_#{m.id}" ] } ]
     }
   end
+
+  def self.all_for_grouped_options_by_literal_year
+    Year.all(:order => "year_desc ASC").collect{ |y|
+      [ y.year_desc, y.months_by_literal_year.collect{ |m| [ m.month_desc, "m_#{m.id}" ] } ]
+    }
+  end
 end
