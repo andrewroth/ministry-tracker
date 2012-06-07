bf3ae9d999936c1d9b80103ed173787954b89b46
Andrew Roth <andrewroth@gmail.com>
2012-06-07 15:51:49 -0400

    add more translations; also make a testing locale with no fallbacks

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,7 +1,7 @@
 I18n.default_locale = "en-CA"
 
 I18n.backend.class.send(:include, I18n::Backend::Fallbacks)
-I18n.fallbacks.map('fr' => 'en-CA')
+I18n.fallbacks[:testing] = []
 
 # force only our English version and French to be available
-I18n.available_locales = [ :"en-CA", "fr" ]
+I18n.available_locales = [ :"en-CA", "fr", "testing" ]
