f881bf50a904a892b84c59ca1e14470a53d51046
Jacques Robitaille <jacques.robitaille@gmail.com>
2012-08-30 16:53:31 -0400

    added permissions for contacts

---
+++ db/migrate/20120830204312_add_contacts_permissions.rb
@@ -0,0 +1,25 @@
+class AddContactsPermissions < ActiveRecord::Migration
+ NEW_PERMISSIONS = [{ :description => "View Contact List", :controller => "contacts", :action => "index" },
+                    { :description => "Edit Contact", :controller => "contacts", :action => "edit" },
+                    { :description => "Batch Edit Contact Assignement", :controller => "contacts", :action => "multiple_assign" },
+                    { :description => "Search Contacts", :controller => "contacts", :action => "search" }]
+
+  def self.find_permission(permission)
+    Permission.find(:first, :conditions => { :controller => permission[:controller], :action => permission[:action] } )
+  end
+  
+  def self.up
+    NEW_PERMISSIONS.each do |permission|
+      Permission.create!(permission) if find_permission(permission).nil?
+    end
+  end
+
+  def self.down
+
+    NEW_PERMISSIONS.each do |permission|
+      p = find_permission(permission)
+      p.destroy unless p.nil?
+    end
+
+  end
+end
