c0f91f75258b006763c463c62677b6a5f7021396
Sheldon Dueck <sheldon.dueck@gmail.com>
2012-09-03 13:27:25 -0400

    Put completed status first in list

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -15,7 +15,7 @@ module ContactsHelper
     {
       :gender_id => [["Male", 2], ["Female", 1], ["Not Specified", 0]],
       :international => [["National", 0], ["International", 1]],
-      :status => [["Uncontacted", 0], ["Attempted", 1], ["Contacted", 4], ["Completed", 2], ["Do Not Contact", 3]],
+      :status => [["Completed", 2], ["Uncontacted", 0], ["Attempted", 1], ["Contacted", 4], ["Do Not Contact", 3]],
       :result => [["No Result Yet", 0], ["Bad Information", 1], ["No Response", 2], ["No Longer Interested", 3], ["Additional Digital Sent", 4], ["Magazine Grab 'n' Go", 5], ["Interaction", 6], ["Interaction & Magazine", 7]]
     }
   end
