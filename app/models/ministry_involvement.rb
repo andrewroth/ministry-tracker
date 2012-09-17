92161506a84f02e23512fcdd2a90244bc300341c
Sheldon Dueck <sheldon.dueck@gmail.com>
2012-09-17 19:38:48 -0400

    Add logging for nil ministry roles

--- app/models/ministry_involvement.rb
+++ app/models/ministry_involvement.rb
@@ -2,4 +2,13 @@ class MinistryInvolvement < ActiveRecord::Base
   load_mappings
   include Common::Core::MinistryInvolvement
   include Common::Core::Ca::MinistryInvolvement
+
+  after_save :check_for_nil_role
+    
+  private
+
+  def check_for_nil_role
+    Rails.logger.info("Detected nil ministry_role_id on involvement #{self.id}") if self.ministry_role_id.nil?
+  end
+
 end
