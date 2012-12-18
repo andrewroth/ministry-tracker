0a6b0f44dfaba5f006ea5b232c7819c0672639a7
Sheldon Dueck <sheldon.dueck@gmail.com>
2012-12-18 11:01:26 -0600

    Increase number of log lines displayed by default

--- app/controllers/connect_controller.rb
+++ app/controllers/connect_controller.rb
@@ -6,14 +6,14 @@ class ConnectController < ApplicationController
     @yml_config_path = CiviCRM::YML_CONFIG_PATH
     config = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join(@yml_config_path))[(ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development')])
 
-    @num_lines = params[:n].try(:to_i) || 1000
+    @num_lines = params[:n].try(:to_i) || 10000
 
     @log_path = config[:log_path] || 'log/civicrm_api.log'
     @log_path = 'log/civicrm_api.log' unless File.exists?(@log_path)
 
     if File.exists?(@log_path)
       tail_output = `tail -n #{@num_lines} #{@log_path}`
-      @unfiltered_log_lines = tail_output.split("\n[").collect { |o| "[#{o}" }
+      @unfiltered_log_lines = tail_output.split("\n[").collect { |o| "#{o[0] == '[' ? '' : '['}#{o}" }
       @include_log_tag = '[IMPORT CONTACTS]'
       @log_lines = @unfiltered_log_lines.select { |line| line.include?(@include_log_tag) }
     end
