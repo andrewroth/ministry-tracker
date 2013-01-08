14f4de1c1d96ec94091eaa5b1a638460e4cb0fd0
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-01-08 10:40:50 -0500

    Add rails env to the import contacts sample command

--- app/controllers/connect_controller.rb
+++ app/controllers/connect_controller.rb
@@ -4,7 +4,8 @@ class ConnectController < ApplicationController
 
   def import_contacts_log
     @yml_config_path = CiviCRM::YML_CONFIG_PATH
-    config = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join(@yml_config_path))[(ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development')])
+    rails_env = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
+    config = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join(@yml_config_path))[rails_env])
 
     @num_lines = params[:n].try(:to_i) || 10000
 
@@ -19,7 +20,7 @@ class ConnectController < ApplicationController
     end
 
     # We just use this to try to check if the import task is in the crontab
-    @crontab_cmd = "flock -n tmp/connect.lock rake connect:import_contacts"
+    @crontab_cmd = "flock -n tmp/connect.lock rake connect:import_contacts RAILS_ENV=#{rails_env}"
     crontab_list_output = `crontab -l`
     # detect a line that includes the cmd and doesn't start with a #
     @cronjob_installed = crontab_list_output && crontab_list_output.split("\n").detect { |line| line.include?(@crontab_cmd) && line !~ /\A\s*#.*\z/ }.present? ? true : false
