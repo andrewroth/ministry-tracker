b1dd205537a29c43b741698408dce6c8e921431f
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-01-10 15:05:55 -0500

    Only cache eventbrite call if eventbright didn't return an error

--- vendor/plugins/eventbright/lib/eventbright/main.rb
+++ vendor/plugins/eventbright/lib/eventbright/main.rb
@@ -2,45 +2,45 @@ require 'httparty'
 require 'tzinfo'
 module EventBright
   EVENTBRITE_TIME_STRING = '%Y-%m-%d %H:%M:%S'
-  
+
   def self.setup_from_initializer()
     self.setup(EventBright::KEYS[:api], EventBright::DEBUG)
     @eventbrite_user ||= EventBright::User.new(EventBright::KEYS[:user])
   end
-  
+
   def self.setup(app_key = "YmRmMmMxMjYzNDYy", debug = false)
     @app_key = app_key
     @debug = debug
   end
-  
+
   def self.debug!
     @debug = true
   end
-  
+
   def self.call(function, opts = {})
     @app_key ||= "YmRmMmMxMjYzNDYy"
     opts[:app_key] = @app_key
-    if opts[:user].is_a? EventBright::User 
+    if opts[:user].is_a? EventBright::User
       # Allow passing User object instead of user auth info.
       u = opts.delete :user
       opts.merge!(u.auth)
-    end 
+    end
     debug "\tSending  /#{function}\t\t(#{opts.inspect})"
     response = API.do_post("/#{function}", :body => opts)
     debug "\tResponse /#{function}\t\t#{response.inspect}"
 
     response
   end
-  
-  
+
+
   def self.debug(msg)
     puts msg if debug?
   end
-  
+
   def self.debug?
     @debug.nil? ? false : @debug
   end
-  
+
   def self.formatted_time(date)
     case date
     when Time
@@ -49,7 +49,7 @@ module EventBright
       Time.parse(String).strftime(EVENTBRITE_TIME_STRING)
     end
   end
-  
+
   class API
     include HTTParty
     base_uri "https://www.eventbrite.com/json/"
@@ -60,9 +60,12 @@ module EventBright
     def self.do_post(function, opts = {})
 
       retries = NUM_RETRIES_ON_404
+      cache_key = "eventbright #{function.inspect} #{opts.inspect} #{Time.now.day} #{Time.now.hour}"
+
       begin
-        response = Rails.cache.fetch("eventbright #{function.inspect} #{opts.inspect} #{Time.now.day} #{Time.now.hour}") { post(function, opts) }
-        Rails.logger.info "\tEventbrite API call (#{Date.today}) \t #{function} \t #{opts.inspect}" if response
+        cached_response = Rails.cache.read(cache_key)
+        Rails.logger.info "\t#{cached_response.present? ? 'Cache hit: ' : ''}Eventbrite API call (#{Date.today}) \t #{function} \t #{opts.inspect}"
+        response = cached_response.present? ? cached_response : post(function, opts)
 
         if response["error"]
           raise Exception.new(response["error"]["error_message"])
@@ -71,6 +74,8 @@ module EventBright
         elsif response.select{|k,v| v =~ /502 Bad Gateway/}.present?
           raise Exception.new(ERROR_502)
         end
+
+        Rails.cache.write(cache_key, response)
 
       rescue Exception => e
         Rails.logger.info "\tEventbright API ERROR \t#{e.message}"
