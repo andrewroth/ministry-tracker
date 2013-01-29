13867fde8642d44cad7744836e8de147dc3ef15a
Colby Warkentin <colby.warkentin@p2c.com>
2013-01-29 17:58:46 -0500

    Added basic editing to the civicrm_schools

--- app/controllers/civicrm_schools_controller.rb
+++ app/controllers/civicrm_schools_controller.rb
@@ -1,5 +1,6 @@
 class CivicrmSchoolsController < ApplicationController
   include CiviCRM
+  before_filter :get_school, :only => [:show, :edit] 
 
   def index
     begin
@@ -14,17 +15,44 @@ class CivicrmSchoolsController < ApplicationController
   end
   
   def show
-     begin
-        connect = CiviCRM::API.new
-        @school = connect.get(:Contact, :with => { :id => params[:id],
-          "return" => "display_name,street_address,city,state_province,postal_code,country,phone,geo_code_1,"+
-          "geo_code_2,custom_68,custom_69,custom_70,custom_71,custom_72,custom_73,custom_74,custom_84,custom_85,"+
-          "custom_86,custom_88,custom_89,custom_90,custom_91,custom_92,custom_93,custom_94,custom_95,"+
-          "custom_96,custom_97,custom_98,custom_99,custom_100,custom_101,custom_102,custom_103,custom_104,custom_105,"+
-          "custom_106,custom_107,custom_108,custom_109,custom_110,custom_111,custom_112,custom_113,custom_114" }).first
-      rescue => e
-        connect.log :error, e
+  end
+
+  def edit
+  end
+
+  def update
+    begin
+      # debugger
+      [:custom_88, :custom_89, :custom_113, :custom_114].each do |link|
+        if !params[:civicrm_schools][link].blank? && !(params[:civicrm_schools][link] =~ /http:\/\/(.*)/)
+          params[:civicrm_schools][link] = "http://" + params[:civicrm_schools][link]
+          debugger
+        end
       end
+
+      connect = CiviCRM::API.new
+      response = connect.update(:Contact, :with => params[:civicrm_schools])
+      response2 = connect.update(:Phone, :with => params[:phone])
+      response3 = connect.create(:Address, :with => params[:address])
+      # debugger
+      if true
+        flash[:notice] = "School Edit Successful!"
+        redirect_to :action => :show
+      else
+        get_school
+        @school.attributes.merge!(params[:civicrm_schools])
+        render :edit
+      end
+    rescue CiviCRM::StandardError => e
+      connect.log :error, e
+      get_school
+      @school.attributes.merge!(params[:civicrm_schools])
+      @school.attributes.merge!(params[:phone])
+      @school.attributes.merge!(params[:address])
+      bad_values = e.message.scan(/value: (.+)\)/).flatten
+      flash[:warning] = "Value \'#{bad_values[0]}\' is not valid"
+      render :edit
+    end
   end
 
   private
@@ -54,5 +82,19 @@ class CivicrmSchoolsController < ApplicationController
       totals[:sponsor] += 1 if school.attr(:custom_74) == 'Sponsor'
     end
     totals
+  end
+
+  def get_school
+    begin
+      connect = CiviCRM::API.new
+      @school = connect.get(:Contact, :with => { :id => params[:id],
+        "return" => "display_name,street_address,city,state_province,postal_code,country,phone,geo_code_1,"+
+        "geo_code_2,custom_68,custom_69,custom_70,custom_71,custom_72,custom_73,custom_74,custom_84,custom_85,"+
+        "custom_86,custom_88,custom_89,custom_90,custom_91,custom_92,custom_93,custom_94,custom_95,"+
+        "custom_96,custom_97,custom_98,custom_99,custom_100,custom_101,custom_102,custom_103,custom_104,custom_105,"+
+        "custom_106,custom_107,custom_108,custom_109,custom_110,custom_111,custom_112,custom_113,custom_114,phone_id,address_id" }).first
+    rescue => e
+      connect.log :error, e
+    end
   end
 end
---
+++ db/migrate/20130129212854_create_civicrm_schools_edit_report_permissions.rb
@@ -0,0 +1,46 @@
+class CreateCivicrmSchoolsEditReportPermissions < ActiveRecord::Migration
+    NEW_PERMISSIONS = [{ :description => "CiviCRM Schools edit", :controller => "civicrm_schools", :action => "edit" },
+                       { :description => "CiviCRM Schools update", :controller => "civicrm_schools", :action => "update" }
+                       ]
+
+    NEW_MINISTRY_ROLE_PERMISSIONS = [{ :ministry_role_name => "Team Member",  :permission_index => 0 },
+                                     { :ministry_role_name => "Team Leader",  :permission_index => 0 },
+
+                                     { :ministry_role_name => "Team Member",  :permission_index => 1 },
+                                     { :ministry_role_name => "Team Leader",  :permission_index => 1 }
+                                     ]
+
+    def self.find_permission(permission)
+      Permission.find(:first, :conditions => { :controller => permission[:controller], :action => permission[:action] } )
+    end
+
+    def self.up
+      NEW_PERMISSIONS.each do |permission|
+        Permission.create!(permission) if find_permission(permission).nil?
+      end
+
+      NEW_MINISTRY_ROLE_PERMISSIONS.each do |mrp|
+        p = NEW_PERMISSIONS[mrp[:permission_index]]
+        mr_id = MinistryRole.find(:first, :conditions => { :name => mrp[:ministry_role_name] } ).id
+        permission_id = Permission.find(:first, :conditions => { :controller => p[:controller], :action => p[:action] } ).id
+        MinistryRolePermission.create!({ :permission_id => permission_id, :ministry_role_id => mr_id })
+      end
+    end
+
+    def self.down
+      NEW_MINISTRY_ROLE_PERMISSIONS.each do |mrp|
+        p = NEW_PERMISSIONS[mrp[:permission_index]]
+        mr = MinistryRole.find(:first, :conditions => { :name => mrp[:ministry_role_name] } )
+        perm = Permission.find(:first, :conditions => { :controller => p[:controller], :action => p[:action] } )
+        unless mr.nil? || perm.nil?
+          mrp = MinistryRolePermission.find(:first, :conditions => { :permission_id => perm.id, :ministry_role_id => mr.id })
+          mrp.destroy unless mrp.nil?
+        end
+      end
+
+      NEW_PERMISSIONS.each do |permission|
+        p = find_permission(permission)
+        p.destroy unless p.nil?
+      end
+    end
+  end
