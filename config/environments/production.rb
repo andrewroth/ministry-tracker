b88a7ea39734c60e3b758e6ec312d645b117d236
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-01-31 13:35:38 -0500

    Fix yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,29 +36,30 @@ if Common::STAGE == "prod" && Common::SERVER == "c4c" && RAILS_ENV == 'productio
 
   mailer_config = YAML.load_file('config/mailer.yml')
 
-  raise "Missing mailer.yml config" if mailer_config[RAILS_ENV][:smtp][:password].to_s.empty? ||
-                                       mailer_config[RAILS_ENV][:smtp][:user_name].to_s.empty? ||
-                                       mailer_config[RAILS_ENV][:smtp][:address].to_s.empty? ||
-                                       mailer_config[RAILS_ENV][:smtp][:domain].to_s.empty? ||
-                                       mailer_config[RAILS_ENV][:smtp][:port].to_s.empty? ||
-                                       mailer_config[RAILS_ENV][:notifier][:app_name].to_s.empty? ||
-                                       mailer_config[RAILS_ENV][:notifier][:subject_prepend].to_s.empty? ||
-                                       mailer_config[RAILS_ENV][:notifier][:sender_address].to_s.empty? ||
-                                       mailer_config[RAILS_ENV][:notifier][:exception_recipients].to_s.empty?
+  raise 'Missing mailer.yml config' if mailer_config[RAILS_ENV]['smtp']['password'].to_s.empty? ||
+                                       mailer_config[RAILS_ENV]['smtp']['user_name'].to_s.empty? ||
+                                       mailer_config[RAILS_ENV]['smtp']['address'].to_s.empty? ||
+                                       mailer_config[RAILS_ENV]['smtp']['domain'].to_s.empty? ||
+                                       mailer_config[RAILS_ENV]['smtp']['port'].to_s.empty? ||
+                                       mailer_config[RAILS_ENV]['notifier']['app_name'].to_s.empty? ||
+                                       mailer_config[RAILS_ENV]['notifier']['subject_prepend'].to_s.empty? ||
+                                       mailer_config[RAILS_ENV]['notifier']['sender_address'].to_s.empty? ||
+                                       mailer_config[RAILS_ENV]['notifier']['exception_recipients'].to_s.empty?
 
   ActionMailer::Base.smtp_settings = {
-    :address   => mailer_config[RAILS_ENV][:smtp][:address],
-    :domain    => mailer_config[RAILS_ENV][:smtp][:domain],
-    :port      => mailer_config[RAILS_ENV][:smtp][:port],
-    :user_name => mailer_config[RAILS_ENV][:smtp][:user_name],
-    :password  => mailer_config[RAILS_ENV][:smtp][:password]
+    :address   => mailer_config[RAILS_ENV]['smtp']['address'],
+    :domain    => mailer_config[RAILS_ENV]['smtp']['domain'],
+    :port      => mailer_config[RAILS_ENV]['smtp']['port'],
+    :user_name => mailer_config[RAILS_ENV]['smtp']['user_name'],
+    :password  => mailer_config[RAILS_ENV]['smtp']['password']
   }
 
   ExceptionNotification::Notifier.configure_exception_notifier do |config|
-    config[:app_name]                 = mailer_config[RAILS_ENV][:notifier][:app_name]
-    config[:subject_prepend]          = mailer_config[RAILS_ENV][:notifier][:subject_prepend]
-    config[:sender_address]           = mailer_config[RAILS_ENV][:notifier][:sender_address]
-    config[:exception_recipients]     = mailer_config[RAILS_ENV][:notifier][:exception_recipients]
+    config[:app_name]                 = mailer_config[RAILS_ENV]['notifier']['app_name']
+    config[:subject_prepend]          = mailer_config[RAILS_ENV]['notifier']['subject_prepend']
+    config[:sender_address]           = mailer_config[RAILS_ENV]['notifier']['sender_address']
+    config[:exception_recipients]     = mailer_config[RAILS_ENV]['notifier']['exception_recipients']
+
     # In a local environment only use this gem to render, never email
     #defaults to false - meaning by default it sends email.  Setting true will cause it to only render the error pages, and NOT email.
     config[:skip_local_notification]  = true
