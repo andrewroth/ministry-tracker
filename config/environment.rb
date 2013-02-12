b52a4f5630d94120e877337767307457f713fdb3
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-02-12 12:58:55 -0500

    Upgrade Rails to 2.3.17

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.17' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -34,8 +34,8 @@ Rails::Initializer.run do |config|
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
   # config.log_level = :debug
-  # config.gem  'json'
-  config.gem  'json_pure', :lib => 'json'
+  # config.gem  'json', :version => '~> 1.7.7'
+  config.gem  'json_pure', :lib => 'json', :version => '~> 1.7.7'
   config.gem  'fastercsv'
   # config.gem  'hpricot', :version => '0.8.3'
   config.gem  'rubyzip', :lib => 'zip/zip'
