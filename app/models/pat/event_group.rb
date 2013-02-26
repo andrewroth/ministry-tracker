6b207562c9b0a232e99c61ed47d68483727bfa5d
Andrew Roth <andrewroth@gmail.com>
2013-02-25 19:17:51 -0500

    add gain student projects to project stats
    a side effect of this is that project stats now only include those who
    have chosen a project and have a campus involvement set

--- app/models/pat/event_group.rb
+++ app/models/pat/event_group.rb
@@ -2,4 +2,5 @@ class Pat::EventGroup < ActiveRecord::Base
   set_inheritance_column "something_you_will_not_use"
   include Common::Pat::EventGroup
   load_mappings
+  has_many :projects, :class_name => "Pat::Project"
 end
