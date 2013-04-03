1661bfa1924fadf14323ba54adc8c0f1a7aa1d3e
Jacques Robitaille <jacques.robitaille@gmail.com>
2013-04-03 17:06:01 -0400

    removed annualgoalsreport

---
+++ db/migrate/20130403192739_remove_table_stats_annual_goals_report.rb
@@ -0,0 +1,26 @@
+class RemoveTableStatsAnnualGoalsReport < ActiveRecord::Migration
+  def self.up
+    drop_table AnnualGoalsReport.table_name
+  end
+
+  def self.down
+    create_table AnnualGoalsReport.table_name, :primary_key => :annualGoalsReport_id do |t|
+      t.integer :annualGoalsReport_id
+      t.integer :campus_id
+      t.integer :year_id
+      t.integer :annualGoalsReport_studInMin, :default => 0
+      t.integer :annualGoalsReport_sptMulti, :default => 0
+      t.integer :annualGoalsReport_firstYears, :default => 0
+      t.integer :annualGoalsReport_summitWent, :default => 0
+      t.integer :annualGoalsReport_wcWent, :default => 0
+      t.integer :annualGoalsReport_projWent, :default => 0
+      t.integer :annualGoalsReport_spConvTotal, :default => 0
+      t.integer :annualGoalsReport_gosPresTotal, :default => 0
+      t.integer :annualGoalsReport_hsPresTotal, :default => 0
+      t.integer :annualGoalsReport_prcTotal, :default => 0
+      t.integer :annualGoalsReport_integBelievers, :default => 0
+      t.integer :annualGoalsReport_lrgEventAttend, :default => 0  
+    end
+  end
+  
+end
